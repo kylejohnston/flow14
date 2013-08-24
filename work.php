b558410da155a0b0408201b51608811840613837
kylejohnston <kyle.johnston@server.fake>
2013-08-24 12:22:08 -0500

    Tweaks to work samples…
    - Moved personal projects under 'Work Samples'
    - Changed layout of 'Clients' area to 3 columns
    - Added 'Client type' to work samples

--- work.php
+++ work.php
@@ -19,7 +19,8 @@
 				<a href="lisa-for-kansas-senate"><img src="img/samples/lisa-for-kansas-senate/thumb.png" alt="Lisa for Kansas Senate"></a>
 				<div class="inner">
 					<h2><a href="lisa-for-kansas-senate">Lisa for Kansas Senate</a></h2>
-					<p>+ <a href="lisa-for-kansas-senate">Read the Case Study</a></p>
+					<p>+ <a href="lisa-for-kansas-senate">Read the Case Study</a><br>
+					Client type: Political</p>
 				</div>
 			</div>
 			<!--<div class="span3 item">
@@ -33,14 +34,16 @@
 				<a href="creative-outfitters"><img src="img/samples/creative-outfitters/thumb.png" width="300" height="150" alt="Creative Outfitters"></a>
 				<div class="inner">
 					<h2><a href="creative-outfitters">Creative Outfitters</a></h2>
-					<p>+ <a href="creative-outfitters">Read the Case Study</a></p>
+					<p>+ <a href="creative-outfitters">Read the Case Study</a><br>
+					Client type: Small business</p>
 				</div>
 			</div>
 			<div class="span4 item">
 				<a href="jocart"><img src="img/samples/jocart/thumb.png" width="300" height="150" alt="JoCART - Johnson County Animal Response Team"></a>
 				<div class="inner">
 					<h2><a href="jocart">JoCART</a></h2>
-					<p>+ <a href="jocart">Read the Case Study</a></p>
+					<p>+ <a href="jocart">Read the Case Study</a><br>
+					Client type: Non-profit</p>
 				</div>
 			</div>
 			<!--<div class="span3 item">
@@ -58,7 +61,8 @@
 				<a href="kansas-dems"><img src="img/samples/kansas-dems/thumb.png" alt="Kansas Dems"></a>
 				<div class="inner">
 					<h2><a href="kansas-dems">Kansas Dems</a></h2>
-					<p>+ <a href="kansas-dems">Read the Case Study</a></p>
+					<p>+ <a href="kansas-dems">Read the Case Study</a><br>
+					Client type: Political</p>
 				</div>
 			</div>
 			<!--<div class="span3 item">
@@ -72,21 +76,24 @@
 				<a href="lisa-for-kansas"><img src="img/samples/lisa-for-kansas/thumb.png" alt="Lisa for U.S. Senate"></a>
 				<div class="inner">
 					<h2><a href="lisa-for-kansas">Lisa for U.S. Senate</a></h2>
-					<p>+ <a href="lisa-for-kansas">Read the Case Study</a></p>
+					<p>+ <a href="lisa-for-kansas">Read the Case Study</a><br>
+					Client type: Political</p>
 				</div>
 			</div>
 			<div class="span3 item">
 				<a href="animal-haven"><img src="img/samples/animalhavenkc/thumb.png" width="300" height="150" alt="Animal Haven"></a>
 				<div class="inner">
 					<h2><a href="animal-haven">Animal Haven</a></h2>
-					<p>+ <a href="animal-haven">Read the Case Study</a></p>
+					<p>+ <a href="animal-haven">Read the Case Study</a><br>
+					Client type: Non-profit</p>
 				</div>
 			</div>
 			<div class="span3 item">
 				<a href="borrowersrep"><img src="img/samples/borrowersrep/thumb.png" width="300" height="150" alt="BorrowersRep.com"></a>
 				<div class="inner">
 					<h2><a href="borrowersrep">BorrowersRep.com</a></h2>
-					<p>+ <a href="borrowersrep">Read the Case Study</a></p>
+					<p>+ <a href="borrowersrep">Read the Case Study</a><br>
+					Client type: Small business</p>
 				</div>
 			</div>
 		</div>
@@ -97,68 +104,81 @@
 				<a href="96kx"><img src="img/samples/96kx/thumb.png" width="300" height="150" alt="96KX"></a>
 				<div class="inner">
 					<h2><a href="96kx">96KX</a></h2>
-					<p>+ <a href="96kx">Read the Case Study</a></p>
+					<p>+ <a href="96kx">Read the Case Study</a><br>
+					Client type: Small business</p>
 				</div>
 			</div>
 			<div class="span4 item">
 				<a href="ibgreek"><img src="img/samples/ibgreek/thumb.png" width="300" height="150" alt="IBGreek"></a>
 				<div class="inner">
 					<h2><a href="ibgreek">IBGreek</a></h2>
-					<p>+ <a href="ibgreek">Read the Case Study</a></p>
+					<p>+ <a href="ibgreek">Read the Case Study</a><br>
+					Client type: Small business</p>
 				</div>
 			</div>
 			<div class="span4 item">
 				<a href="harmony-hearing-aids"><img src="img/samples/harmony/thumb.png" width="300" height="150" alt="Harmony Hearing Aids"></a>
 				<div class="inner">
 					<h2><a href="harmony-hearing-aids">Harmony Hearing Aids</a></h2>
-					<p>+ <a href="harmony-hearing-aids">Read the Case Study</a></p>
+					<p>+ <a href="harmony-hearing-aids">Read the Case Study</a><br>
+					Client type: Small business</p>
 				</div>
 			</div>
 		</div>
 	</div><!-- Row 3 -->
+	<div class="container items">
+		<h2>Personal Projects</h2>
+		<div class="row">
+			<div class="span4 item">
+				<a href="iphone-365"><img src="img/samples/iphone-365/thumb.png" alt="iPhone 365"></a>
+				<div class="inner">
+					<h2><a href="iphone-365">iPhone 365</a></h2>
+					<p>+ <a href="iphone-365">Watch the Videos</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<div class="container">
 		<div class="row">
 			<hr>
-			<div class="span8">
-				<h2>Clients</h2>
+			<div class="span12">
 				<div class="row">
 					<div class="span4">
-						<p><strong>Small Businesses</strong><br>
-						BorrowersRep.com<br>
-						<a href="http://www.campaignandwin.com/">CampaignAndWin.com</a><br>
-						<a href="http://www.creativeoutfitters.com/">Creative Outfitters</a><br>
-						Harmony Hearing Aids<br>
-						IBGreek<br>
-						<a href="http://www.mariposavet.com/">Mariposa Veterinary Wellness Clinic</a><br>
-						96KX</p>
-						<p><strong>Education</strong><br>
-						<a href="http://www.bakeru.edu">Baker University</a><br>
-						<a href="http://www.kcpdc.org">KCPDC</a></p>
+						<div class="inner">
+							<h2>Clients</h2>
+							<p><strong>Small Businesses</strong><br>
+							BorrowersRep.com<br>
+							<a href="http://www.campaignandwin.com/">CampaignAndWin.com</a><br>
+							<a href="http://www.creativeoutfitters.com/">Creative Outfitters</a><br>
+							Harmony Hearing Aids<br>
+							IBGreek<br>
+							<a href="http://www.mariposavet.com/">Mariposa Veterinary Wellness Clinic</a><br>
+							96KX</p>
+						</div>
 					</div>
 					<div class="span4">
-						<p><strong>Non-Profits</strong><br>
-						Animal Haven<br>
-						<a href="http://www.greatplainsspca.org/">Great Plains SPCA</a><br>
-						<a href="http://jocartks.org/">JoCART</a><br>
-						<a href="http://kcpetproject.org/">KC Pet Project</a><br>
-						Siraba School<br>
-						<a href="http://www.waysidewaifs.org/">Wayside Waifs</a></p>
-						<p><strong>Political</strong><br>
-						<a href="http://www.kansasdems.org/">Kansas Dems</a><br>
-						<a href="http://www.lisaforkansas.com">Lisa for Kansas</a><br>
-						Dennis McKinney<br>
-						<a href="http://www.sapphirewire.com">Sapphire Wire</a></p>
+						<div class="inner">
+							<p><strong>Non-Profits</strong><br>
+							Animal Haven<br>
+							<a href="http://www.greatplainsspca.org/">Great Plains SPCA</a><br>
+							<a href="http://jocartks.org/">JoCART</a><br>
+							<a href="http://kcpetproject.org/">KC Pet Project</a><br>
+							Siraba School<br>
+							<a href="http://www.waysidewaifs.org/">Wayside Waifs</a></p>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div class="span4">
-				<h2>Personal Projects</h2>
-				<div class="item">
-					<a href="iphone-365"><img src="img/samples/iphone-365/thumb.png" alt="iPhone 365"></a>
-					<div class="inner">
-						<h2><a href="iphone-365">iPhone 365</a></h2>
-						<p>+ <a href="iphone-365">Watch the Videos</a></p>
+					<div class="span4">
+						<div class="inner">
+							<p><strong>Political</strong><br>
+							<a href="http://www.kansasdems.org/">Kansas Dems</a><br>
+							<a href="http://www.lisaforkansas.com">Lisa for Kansas</a><br>
+							Dennis McKinney<br>
+							<a href="http://www.sapphirewire.com">Sapphire Wire</a></p>
+							<p><strong>Education</strong><br>
+							<a href="http://www.bakeru.edu">Baker University</a><br>
+							<a href="http://www.kcpdc.org">KCPDC</a></p>
+						</div>
 					</div>
 				</div>
 			</div>
