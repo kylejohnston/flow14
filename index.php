f8e03eb94d3b49b40db716d9120c4c22977f18b0
kylejohnston <kyle.johnston@server.fake>
2013-10-06 12:29:27 -0500

    SEO Optimization
    - Updated title tag
    - Updated meta description
    - Updated meta keywords
    - Updated homepage intro copy

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>flow14 - Design Works.</title>
+	<title>flow14 - Design Works | Overland Park, Kansas Graphic & Web Design</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="flow14 is a design studio that specializes in helping small businesses, non-profits, and political groups connect with customers through engaging websites, logo design, collateral material and more.">
-	<meta name="keywords" content="graphic design, web design, business cards, website, logo design, overland park, kansas city">
+	<meta name="description" content="Based in Overland Park, Kansas, flow14 helps small businesses, non-profits, and political groups stand out with exceptional graphic design and web design.">
+	<meta name="keywords" content="graphic design, web design, business cards, website, logo design, overland park, kansas, kansas city">
 	<meta name="author" content="Kyle Johnston">
 	<?php include('includes/head.php'); ?>
 </head>
 <body>
 		<div id="fresh">
 			<img src="img/flow14-logo_lg.png" alt="flow14 - Design Works.">
-			<p>We create outstanding graphic design to help your small business, <nobr>non-profit</nobr>, or political group connect with your audience.</p>
+			<p>Based in Overland Park, Kansas, flow14 helps your small business, <nobr>non-profit</nobr>, or political group connect with your audience with exceptional graphic design and web design.</p>
 			<p>See <a href="work">samples of our work</a>, learn more <a href="about">about us</a> or <a href="contact">contact us</a> for a quote today.</p>
 		</div>
 	<div class="container marketing">
