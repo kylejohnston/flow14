2b521bfda2bf648e1c4052ecf916ae9d5f21604f
kylejohnston <kyle.johnston@server.fake>
2013-10-27 17:17:00 -0500

    Changed CTA
    Before: 'Read the case study'
    After: 'View addtional samples…'

--- work.php
+++ work.php
@@ -19,7 +19,7 @@
 				<a href="creative-outfitters"><img src="img/samples/creative-outfitters/thumb.png" width="300" height="150" alt="Creative Outfitters"></a>
 				<div class="inner">
 					<h2><a href="creative-outfitters">Creative Outfitters</a></h2>
-					<p>+ <a href="creative-outfitters">Read the Case Study</a><br>
+					<p>+ <a href="creative-outfitters">View additional samples…</a><br>
 					Client type: Small business</p>
 				</div>
 			</div>
@@ -27,7 +27,7 @@
 				<a href="borrowersrep"><img src="img/samples/borrowersrep/thumb.png" width="300" height="150" alt="BorrowersRep.com"></a>
 				<div class="inner">
 					<h2><a href="borrowersrep">BorrowersRep.com</a></h2>
-					<p>+ <a href="borrowersrep">Read the Case Study</a><br>
+					<p>+ <a href="borrowersrep">View additional samples…</a><br>
 					Client type: Small business</p>
 				</div>
 			</div>
@@ -35,7 +35,7 @@
 				<a href="lisa-for-kansas-senate"><img src="img/samples/lisa-for-kansas-senate/thumb.png" alt="Lisa for Kansas Senate"></a>
 				<div class="inner">
 					<h2><a href="lisa-for-kansas-senate">Lisa for Kansas Senate</a></h2>
-					<p>+ <a href="lisa-for-kansas-senate">Read the Case Study</a><br>
+					<p>+ <a href="lisa-for-kansas-senate">View additional samples…</a><br>
 					Client type: Political</p>
 				</div>
 			</div>
@@ -61,7 +61,7 @@
 				<a href="kansas-dems"><img src="img/samples/kansas-dems/thumb.png" alt="Kansas Dems"></a>
 				<div class="inner">
 					<h2><a href="kansas-dems">Kansas Dems</a></h2>
-					<p>+ <a href="kansas-dems">Read the Case Study</a><br>
+					<p>+ <a href="kansas-dems">View additional samples…</a><br>
 					Client type: Political</p>
 				</div>
 			</div>
@@ -76,7 +76,7 @@
 				<a href="lisa-for-kansas"><img src="img/samples/lisa-for-kansas/thumb.png" alt="Lisa for U.S. Senate"></a>
 				<div class="inner">
 					<h2><a href="lisa-for-kansas">Lisa for U.S. Senate</a></h2>
-					<p>+ <a href="lisa-for-kansas">Read the Case Study</a><br>
+					<p>+ <a href="lisa-for-kansas">View additional samples…</a><br>
 					Client type: Political</p>
 				</div>
 			</div>
@@ -84,7 +84,7 @@
 				<a href="animal-haven"><img src="img/samples/animalhavenkc/thumb.png" width="300" height="150" alt="Animal Haven"></a>
 				<div class="inner">
 					<h2><a href="animal-haven">Animal Haven</a></h2>
-					<p>+ <a href="animal-haven">Read the Case Study</a><br>
+					<p>+ <a href="animal-haven">View additional samples…</a><br>
 					Client type: Non-profit</p>
 				</div>
 			</div>
@@ -92,7 +92,7 @@
 				<a href="jocart"><img src="img/samples/jocart/thumb.png" width="300" height="150" alt="JoCART - Johnson County Animal Response Team"></a>
 				<div class="inner">
 					<h2><a href="jocart">JoCART</a></h2>
-					<p>+ <a href="jocart">Read the Case Study</a><br>
+					<p>+ <a href="jocart">View additional samples…</a><br>
 					Client type: Non-profit</p>
 				</div>
 			</div>
@@ -104,7 +104,7 @@
 				<a href="96kx"><img src="img/samples/96kx/thumb.png" width="300" height="150" alt="96KX"></a>
 				<div class="inner">
 					<h2><a href="96kx">96KX</a></h2>
-					<p>+ <a href="96kx">Read the Case Study</a><br>
+					<p>+ <a href="96kx">View additional samples…</a><br>
 					Client type: Small business</p>
 				</div>
 			</div>
@@ -112,7 +112,7 @@
 				<a href="ibgreek"><img src="img/samples/ibgreek/thumb.png" width="300" height="150" alt="IBGreek"></a>
 				<div class="inner">
 					<h2><a href="ibgreek">IBGreek</a></h2>
-					<p>+ <a href="ibgreek">Read the Case Study</a><br>
+					<p>+ <a href="ibgreek">View additional samples…</a><br>
 					Client type: Small business</p>
 				</div>
 			</div>
@@ -120,7 +120,7 @@
 				<a href="harmony-hearing-aids"><img src="img/samples/harmony/thumb.png" width="300" height="150" alt="Harmony Hearing Aids"></a>
 				<div class="inner">
 					<h2><a href="harmony-hearing-aids">Harmony Hearing Aids</a></h2>
-					<p>+ <a href="harmony-hearing-aids">Read the Case Study</a><br>
+					<p>+ <a href="harmony-hearing-aids">View additional samples…</a><br>
 					Client type: Small business</p>
 				</div>
 			</div>
