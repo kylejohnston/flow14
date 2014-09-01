aa0a5fbae253e1207a280e0e30e784ee3652b6d8
kylejohnston <kyle.johnston@server.fake>
2014-09-01 02:13:37 -0500

    Updated link to work
    Now points to homepage anchor

--- includes/header-nav.php
+++ includes/header-nav.php
@@ -2,7 +2,7 @@
 		<div class="row">
 			<div class="span12 nav">
 				<div class="logo"><a href="index"><span itemprop="name">flow14</span></a></div>
-				<div><a href="work">Work</a></div>
+				<div><a href="/#work">Work</a></div>
 				<div><a href="about">About</a></div>
 				<div><a href="contact">Contact</a></div>
 			</div>
