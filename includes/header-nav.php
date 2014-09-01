5e7f3f2598df49d1b9a105ad4c36eb9ef5e23149
kylejohnston <kyle.johnston@server.fake>
2014-09-01 02:41:13 -0500

    Revised header nav layout
    Made nav items float right

--- includes/header-nav.php
+++ includes/header-nav.php
@@ -1,10 +1,14 @@
 	<div class="container">
 		<div class="row">
-			<div class="span12 nav">
-				<div class="logo"><a href="index"><span itemprop="name">flow14</span></a></div>
+			<div class="span2 logo">
+				<div><a href="index"><span itemprop="name">flow14</span></a></div>
+			</div>
+			<div class="span10 nav">
+				<div class="nav-wrap">
 				<div><a href="/#work">Work</a></div>
 				<div><a href="about">About</a></div>
 				<div><a href="contact">Contact</a></div>
+				</div>
 			</div>
 		</div>
 	</div>
