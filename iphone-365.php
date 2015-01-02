42b2ee82ac8b4b0fd065c7159aaad00b32c5d14f
kylejohnston <kyle.johnston@server.fake>
2015-01-02 15:02:09 -0600

    Added 2014 video, edited description

--- iphone-365.php
+++ iphone-365.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Personal Project: iPhone 365 |  flow14</title>
+	<title>Personal Project: iPhone 365 Project |  flow14</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="iPhone 365: take a photo a day, every day.">
-	<meta name="keywords" content="iphone 365, photography">
+	<meta name="description" content="iPhone 365 Project: take a photo a day, every day.">
+	<meta name="keywords" content="iphone 365, 365 project, photography">
 	<meta name="author" content="Kyle Johnston">
 	<?php include('includes/head.php'); ?>
 </head>
@@ -16,17 +16,35 @@
 		<div class="container">
 			<div class="row">
 				<div class="introduction">
-					<h1>iPhone 365</h1>
-					<p><strong>The Story.</strong> Since 2009, I’ve participated in <a href="http://www.flickr.com/groups/iphone365/">iPhone 365</a>, a project created by a couple of friends who challenged themselves to take a photo a day, every day. At the end of each year, I’ve strung my photos together to create a video, set to the song I listened to the most during the year.</p>
-					<p><strong>Stats.</strong> 2009: 212 photos, 2010: 280 photos, 2011: 365 photos, 2012: 366 photos (leap year), 2013: 365 photos</p>
+					<h1>iPhone 365 Project</h1>
+					<p><strong>The Story.</strong> Since 2009, I’ve participated in <a href="http://www.flickr.com/groups/iphone365/">iPhone 365</a>, a project created by a couple of friends who challenged themselves to take a photo a day, every day. At the end of each year, I’ve compiled my photos into a short video, set to the song I listened to the most during the year.</p>
+					<p><strong>Stats.</strong>1,953 photos since 2009. Year-by-year: 2009: 212 photos, 2010: 280 photos, 2011: 365 photos, 2012: 366 photos (leap year), 2013: 365 photos, 2014: 365 photos</p>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<div class="container marketing">
-		<!-- 2013 -->
+		<!-- 2014 -->
 		<div class="featurette">
+			<div class="container">
+				<div class="row">
+					<div class="span3">
+						<div class="inner" style="text-align: center;">
+							<h2>2014</h2>
+						</div>
+					</div>
+					<div class="span9 video">
+						<div class="inner">
+							<iframe src="http://player.vimeo.com/video/115783465?byline=0&amp;portrait=0&amp;color=c9ff23" width="600" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- 2013 -->
+		<div class="featurette featurette-alt">
 			<div class="container">
 				<div class="row">
 					<div class="span3">
@@ -44,7 +62,7 @@
 		</div>
 
 		<!-- 2012 -->
-		<div class="featurette featurette-alt">
+		<div class="featurette">
 			<div class="container">
 				<div class="row">
 					<div class="span3">
@@ -62,7 +80,7 @@
 		</div>
 
 		<!-- 2011 -->
-		<div class="featurette">
+		<div class="featurette featurette-alt">
 			<div class="container">
 				<div class="row">
 					<div class="span3">
@@ -80,7 +98,7 @@
 		</div>
 
 		<!-- 2010 -->
-		<div class="featurette featurette-alt">
+		<div class="featurette">
 			<div class="container">
 				<div class="row">
 					<div class="span3">
@@ -98,7 +116,7 @@
 		</div>
 
 		<!-- 2009 -->
-		<div class="featurette">
+		<div class="featurette featurette-alt">
 			<div class="container">
 				<div class="row">
 					<div class="span3">
