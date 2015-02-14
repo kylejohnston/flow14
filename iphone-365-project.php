06bbfd2ab3a5f6fd36e874d85db07d9ac4b891c4
kylejohnston <kyle.johnston@server.fake>
2015-02-14 05:25:10 -0600

    Updated layout, naming convention
    - Added canonical meta tag
    - Added production notes to 2014 + 2013 videos
    - Adjusted layout for all videos

--- iphone-365-project.php
+++ iphone-365-project.php
@@ -7,6 +7,8 @@
 	<meta name="description" content="iPhone 365 Project: take a photo a day, every day.">
 	<meta name="keywords" content="iphone 365, 365 project, photography">
 	<meta name="author" content="Kyle Johnston">
+	<link rel="canonical" href="http://www.flow14.com/iphone-365-project">
+
 	<?php include('includes/head.php'); ?>
 </head>
 
@@ -29,12 +31,18 @@
 		<div class="featurette">
 			<div class="container">
 				<div class="row">
-					<div class="span3">
-						<div class="inner" style="text-align: center;">
+					<div class="span5">
+						<div class="inner">
 							<h2>2014</h2>
+							<p><strong>Production notes:</strong><br>
+							All photos & video were shot with an iPhone 5<br>
+							<strong>Apps - Shooting:</strong> Camera (default), <a href="https://itunes.apple.com/us/app/id357404131">SlowShutter</a> (long exposures)<br>
+							<strong>Apps - Editing:</strong> <a href="https://itunes.apple.com/us/app/vsco-cam/id588013838?mt=8">VSCOcam</a>, <a href="https://itunes.apple.com/us/app/skrwt/id834248867?mt=8">SKRWT</a>, <a href="https://itunes.apple.com/us/app/image-blender/id414544492?mt%3D8">Image Blender</a><br>
+							<strong>365 Video:</strong> Downloaded photos from my <a href="https://www.flickr.com/photos/flow14/sets/72157639294916334/">365 Flickr album</a> using <a href="http://downloadair.ghusse.com">DownloadAir</a> then compiled and edited the video with Adobe Premiere.<br>
+							<strong>Music:</strong> Set to <em>"Nous étions deux"</em> by La Femme</p>
 						</div>
 					</div>
-					<div class="span9 video">
+					<div class="span7 video">
 						<div class="inner">
 							<iframe src="http://player.vimeo.com/video/115783465?byline=0&amp;portrait=0&amp;color=c9ff23" width="600" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 						</div>
@@ -47,14 +55,20 @@
 		<div class="featurette featurette-alt">
 			<div class="container">
 				<div class="row">
-					<div class="span3">
-						<div class="inner" style="text-align: center;">
-							<h2>2013</h2>
-						</div>
-					</div>
-					<div class="span9 video">
+					<div class="span7 video">
 						<div class="inner">
 							<iframe src="http://player.vimeo.com/video/83177011?byline=0&amp;portrait=0&amp;color=c9ff23" width="600" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+						</div>
+					</div>
+					<div class="span5">
+						<div class="inner">
+							<h2>2013</h2>
+							<p><strong>Production Notes:</strong><br>
+							All photos & video were shot with an iPhone 5<br>
+							<strong>Apps - Shooting:</strong> Camera (default), <a href="https://itunes.apple.com/us/app/id357404131">SlowShutter</a> (long exposures), SnappyCam (for action)<br>
+							<strong>Apps - Editing:</strong> <a href="https://itunes.apple.com/us/app/vsco-cam/id588013838?mt=8">VSCOcam</a>, Camera+, <a href="https://itunes.apple.com/us/app/image-blender/id414544492?mt%3D8">Image Blender</a><br>
+							<strong>365 Video:</strong> Downloaded photos from my <a href="http://www.flickr.com/photos/flow14/sets/72157632410070343/">2013 365 Flickr set</a> using <a href="http://www.malarkeysoftware.com/projects_PhotoGrabbr.html">Photo Grabbr</a> then compiled and edited everything with Adobe Premiere.<br>
+							<strong>Music:</strong> Set to <em>Manhattan</em> by Cat Power</p>
 						</div>
 					</div>
 				</div>
@@ -65,12 +79,12 @@
 		<div class="featurette">
 			<div class="container">
 				<div class="row">
-					<div class="span3">
+					<div class="span6">
 						<div class="inner" style="text-align: center;">
 							<h2>2012 <span class="caption">(Leap Year)</span></h2>
 						</div>
 					</div>
-					<div class="span9 video">
+					<div class="span6 video">
 						<div class="inner">
 							<iframe src="http://player.vimeo.com/video/56615870?byline=0&amp;portrait=0&amp;color=c9ff23" width="600" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 						</div>
@@ -83,12 +97,12 @@
 		<div class="featurette featurette-alt">
 			<div class="container">
 				<div class="row">
-					<div class="span3">
+					<div class="span6">
 						<div class="inner" style="text-align: center;">
 							<h2>2011</h2>
 						</div>
 					</div>
-					<div class="span9 video">
+					<div class="span6 video">
 						<div class="inner">
 							<iframe src="http://player.vimeo.com/video/34446500?byline=0&amp;portrait=0&amp;color=c9ff23" width="600" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 						</div>
@@ -101,12 +115,12 @@
 		<div class="featurette">
 			<div class="container">
 				<div class="row">
-					<div class="span3">
+					<div class="span6">
 						<div class="inner" style="text-align: center;">
 							<h2>2010</h2>
 						</div>
 					</div>
-					<div class="span9 video">
+					<div class="span6 video">
 						<div class="inner">
 							<iframe src="http://player.vimeo.com/video/18376909?byline=0&amp;portrait=0&amp;color=c9ff23" width="600" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 						</div>
@@ -119,12 +133,12 @@
 		<div class="featurette featurette-alt">
 			<div class="container">
 				<div class="row">
-					<div class="span3">
+					<div class="span6">
 						<div class="inner" style="text-align: center;">
 							<h2>2009</h2>
 						</div>
 					</div>
-					<div class="span9 video">
+					<div class="span6 video">
 						<div class="inner">
 							<iframe src="http://player.vimeo.com/video/8490705?byline=0&amp;portrait=0&amp;color=c9ff23" width="600" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 						</div>
