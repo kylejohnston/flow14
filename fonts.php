aeaf29505f635a7c18d1ff6279af1cd19c3199b5
kylejohnston <kyle.johnston@server.fake>
2015-02-15 12:04:57 -0600

    Improved image quality, updated Mac Font files
    - Generated new, retina-quality images
    - Updated Mac font files – changed SIT format to ZIP
    - Updated Pinterest buttons

--- fonts.php
+++ fonts.php
@@ -15,24 +15,24 @@
 		<h1>Free Fonts</h1>
 		<div class="row">
 			<div class="span4 item">
-				<img src="img/fonts/sporty.png" alt="Free Font - Sporty">
+				<img src="img/fonts/sporty.2x.png" alt="Free Font - Sporty">
 				<div class="inner">
-					<p><strong>Sporty</strong> is a collegiate-style display font. All caps with minimal punctuation.<br><a href="http://www.flow14.com/font-files/spomacps.sit" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Sporty-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/spopctt.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Sporty-PC']);">Download - PC</a></p>
+					<p><strong>Sporty</strong> is a collegiate-style display font. All caps with minimal punctuation.<br><a href="http://www.flow14.com/font-files/sporty.zip" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Sporty-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/spopctt.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Sporty-PC']);">Download - PC</a></p>
 					<p><a href="//pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flow14.com%2Ffonts&media=http%3A%2F%2Fwww.flow14.com%2Fimg%2Ffonts%2Fsporty_pin.png&description=Sporty%20-%20Free%20 Font%20-%20A%20collegiate-style%20display%20font" data-pin-do="buttonPin" data-pin-config="beside"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></p> 
 				</div>
 			</div>
 			<div class="span4 item">
-				<img src="img/fonts/bodolive.png" alt="Free Font - Bodolive">
+				<img src="img/fonts/bodolive.2x.png" alt="Free Font - Bodolive">
 				<div class="inner">
-					<p><strong>Bodolive</strong> is a mutation of two heavyweight display fonts. Caps, lowercase and punctuation.<br><a href="http://www.flow14.com/font-files/bodmacps.sit" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Bodolive-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/bodpctt.zip" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Bodolive-PC']);">Download - PC</a></p>
-					<p><a href="//pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flow14.com%2Ffonts&media=http%3A%2F%2Fwww.flow14.com%2Fimg%2Ffonts%2Fbodolive_pin.png&description=Bodolive%20-%20Free%20 Font%20-%20A%20mutation%20of%20two%20heavyweight%20display%20fonts" data-pin-do="buttonPin" data-pin-config="beside"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></p> 
+					<p><strong>Bodolive</strong> is a mutation of two heavyweight display fonts. Caps, lowercase and punctuation.<br><a href="http://www.flow14.com/font-files/bodolive.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Bodolive-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/bodpctt.zip" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Bodolive-PC']);">Download - PC</a></p>
+					<p><a href="//pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flow14.com%2Ffonts&media=http%3A%2F%2Fwww.flow14.com%2Fimg%2Ffonts%2Fbodolive.2x.png&description=Bodolive%20-%20Free%20 Font%20-%20A%20mutation%20of%20two%20heavyweight%20display%20fonts" data-pin-do="buttonPin" data-pin-config="beside"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></p> 
 				</div>
 			</div>
 			<div class="span4 item">
-				<img src="img/fonts/meteors.png" alt="Free Font - Meteors">
+				<img src="img/fonts/meteors.2x.png" alt="Free Font - Meteors">
 				<div class="inner">
-					<p><strong>Meteors</strong> is a font based on type from the classic video game Asteroids. Caps, lowercase and punctuation.<br><a href="http://www.flow14.com/font-files/metmacps.sit" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Meteors-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/metpctt.zip" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Meteors-PC']);">Download - PC</a></p>
-					<p><a href="//pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flow14.com%2Ffonts&media=http%3A%2F%2Fwww.flow14.com%2Fimg%2Ffonts%2Fmeteors_pin.png&description=Meteors%20-%20Free%20 Font%20-%20A%20font%20based%20on%20type%20from%20the%20classic%20video%20game%20Asteroids" data-pin-do="buttonPin" data-pin-config="beside"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></p> 
+					<p><strong>Meteors</strong> is a font based on type from the classic video game Asteroids. Caps, lowercase and punctuation.<br><a href="http://www.flow14.com/font-files/meteors.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Meteors-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/metpctt.zip" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Meteors-PC']);">Download - PC</a></p>
+					<p><a href="//pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flow14.com%2Ffonts&media=http%3A%2F%2Fwww.flow14.com%2Fimg%2Ffonts%2Fmeteors.2x.png&description=Meteors%20-%20Free%20 Font%20-%20A%20font%20based%20on%20type%20from%20the%20classic%20video%20game%20Asteroids" data-pin-do="buttonPin" data-pin-config="beside"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></p> 
 				</div>
 			</div>
 		</div>
@@ -40,10 +40,10 @@
 	<div class="container items">
 		<div class="row">
 			<div class="span4 item">
-				<img src="img/fonts/jellyphant.png" alt="Free Font - jellyphant">
+				<img src="img/fonts/jellyphant.2x.png" alt="Free Font - jellyphant">
 				<div class="inner">
-					<p><strong>jellyphant</strong> is an all lowercase display font &#8212; no caps, no numbers, no punctuation, just lil’ letters.<br><a href="http://www.flow14.com/font-files/jelmacps.sit" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Jellyphant-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/jelpctt.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Jellyphant-PC']);">Download - PC</a></p>
-					<p><a href="//pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flow14.com%2Ffonts&media=http%3A%2F%2Fwww.flow14.com%2Fimg%2Ffonts%2Fjellyphant_pin.png&description=Jellyphant%20-%20Free%20 Font%20-%20An%20all%20lowercase%20display%20font" data-pin-do="buttonPin" data-pin-config="beside"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></p> 
+					<p><strong>jellyphant</strong> is an all lowercase display font &#8212; no caps, no numbers, no punctuation, just lil’ letters.<br><a href="http://www.flow14.com/font-files/jellyphant.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Jellyphant-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/jelpctt.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Jellyphant-PC']);">Download - PC</a></p>
+					<p><a href="//pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flow14.com%2Ffonts&media=http%3A%2F%2Fwww.flow14.com%2Fimg%2Ffonts%2Fjellyphant.2x.png&description=Jellyphant%20-%20Free%20 Font%20-%20An%20all%20lowercase%20display%20font" data-pin-do="buttonPin" data-pin-config="beside"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a></p> 
 				</div>
 			</div>
 		</div>
