3ba2d6220381e5d87979d30ed796643e12133b71
kylejohnston <kylejohnston@users.noreply.github.com>
2015-04-02 10:08:30 -0500

    Added new project & made other improvements…
    Other improvements…
    - Updated all captions (added services)
    - Fixed Siraba School link
    - Added client: Reggie Marselus
    - Updated Ron Worley’s listing

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 				</div>
 			</div>
 		</div>
-<!-- thumbnail computed size: 370x185 -->
+<!-- thumbnail computed size: 370x185; 2x: 740x370 -->
 	<div class="container items">
 		<a name="work"></a>
 		<h1>Work Samples</h1>
@@ -30,23 +30,21 @@
 				<a href="creative-outfitters"><img src="img/samples/creative-outfitters/thumb.png" width="300" height="150" alt="Creative Outfitters"></a>
 				<div class="inner">
 					<h2><a href="creative-outfitters">Creative Outfitters</a></h2>
-					<p>+ <a href="creative-outfitters">View additional samples…</a><br>
-					Client type: Small business</p>
+					<p>Brand identity, business cards, &amp; website<br>Client type: Small business</p>
 				</div>
 			</div>
 			<div class="span4 item">
-				<a href="borrowersrep"><img src="img/samples/borrowersrep/thumb.2x.png" width="300" height="150" alt="BorrowersRep.com"></a>
+				<a href="kc-lisc"><img src="img/samples/kc-lisc/thumb.2x.jpg" width="300" height="150" alt="Kansas City LISC"></a>
 				<div class="inner">
-					<h2><a href="borrowersrep">BorrowersRep.com</a></h2>
-					<p>+ <a href="borrowersrep">View additional samples…</a><br>
-					Client type: Small business</p>
+					<h2><a href="kc-lisc">Kansas City LISC</a></h2>
+					<p>Infographic &amp; data visualization<br>Client type: Non-profit</p>
 				</div>
 			</div>
 			<div class="span4 item">
 				<a href="lisa-for-kansas-senate"><img src="img/samples/lisa-for-kansas-senate/thumb.2x.png" alt="Lisa for Kansas Senate"></a>
 				<div class="inner">
 					<h2><a href="lisa-for-kansas-senate">Lisa for Kansas Senate</a></h2>
-					<p>+ <a href="lisa-for-kansas-senate">View additional samples…</a><br>
+					<p>Brand identity, yard signs, &amp; website<br>
 					Client type: Political</p>
 				</div>
 			</div>
@@ -58,7 +56,7 @@
 				<a href="websites"><img src="img/samples/websites/thumbnail.2x.jpg" width="300" height="150" alt="Responsive Website Designs"></a>
 				<div class="inner">
 					<h2><a href="websites">Responsive Websites</a></h2>
-					<p>+ <a href="websites">View additional samples…</a><br>
+					<p>Responsive website design &amp; development<br>
 					Client type: Various</p>
 				</div>
 			</div>
@@ -66,7 +64,7 @@
 				<a href="kansas-dems"><img src="img/samples/kansas-dems/thumb.2x.png" alt="Kansas Dems"></a>
 				<div class="inner">
 					<h2><a href="kansas-dems">Kansas Dems</a></h2>
-					<p>+ <a href="kansas-dems">View additional samples…</a><br>
+					<p>Brand identity<br>
 					Client type: Political</p>
 				</div>
 			</div>
@@ -74,7 +72,7 @@
 				<a href="lisa-for-kansas"><img src="img/samples/lisa-for-kansas/thumb.2x.png" alt="Lisa for U.S. Senate"></a>
 				<div class="inner">
 					<h2><a href="lisa-for-kansas">Lisa for U.S. Senate</a></h2>
-					<p>+ <a href="lisa-for-kansas">View additional samples…</a><br>
+					<p>Logo design, website, printed materials, & TV spots<br>
 					Client type: Political</p>
 				</div>
 			</div>
@@ -82,7 +80,7 @@
 				<a href="animal-haven"><img src="img/samples/animalhavenkc/thumb.png" width="300" height="150" alt="Animal Haven"></a>
 				<div class="inner">
 					<h2><a href="animal-haven">Animal Haven</a></h2>
-					<p>+ <a href="animal-haven">View additional samples…</a><br>
+					<p>Website redesign<br>
 					Client type: Non-profit</p>
 				</div>
 			</div>
@@ -94,7 +92,7 @@
 				<a href="jocart"><img src="img/samples/jocart/thumb.2x.png" width="300" height="150" alt="JoCART - Johnson County Animal Response Team"></a>
 				<div class="inner">
 					<h2><a href="jocart">JoCART</a></h2>
-					<p>+ <a href="jocart">View additional samples…</a><br>
+					<p>Brand identity, website<br>
 					Client type: Non-profit</p>
 				</div>
 			</div>
@@ -102,7 +100,7 @@
 				<a href="96kx"><img src="img/samples/96kx/thumb.png" width="300" height="150" alt="96KX"></a>
 				<div class="inner">
 					<h2><a href="96kx">96KX</a></h2>
-					<p>+ <a href="96kx">View additional samples…</a><br>
+					<p>Logo design<br>
 					Client type: Small business</p>
 				</div>
 			</div>
@@ -110,7 +108,7 @@
 				<a href="ibgreek"><img src="img/samples/ibgreek/thumb.png" width="300" height="150" alt="IBGreek"></a>
 				<div class="inner">
 					<h2><a href="ibgreek">IBGreek</a></h2>
-					<p>+ <a href="ibgreek">View additional samples…</a><br>
+					<p>Brand identity, business cards<br>
 					Client type: Small business</p>
 				</div>
 			</div>
@@ -118,7 +116,7 @@
 				<a href="harmony-hearing-aids"><img src="img/samples/harmony/thumb.2x.png" width="300" height="150" alt="Harmony Hearing Aids"></a>
 				<div class="inner">
 					<h2><a href="harmony-hearing-aids">Harmony Hearing Aids</a></h2>
-					<p>+ <a href="harmony-hearing-aids">View additional samples…</a><br>
+					<p>Brand identity, business cards<br>
 					Client type: Small business</p>
 				</div>
 			</div>
@@ -176,8 +174,9 @@
 							Animal Haven<br>
 							<a href="http://www.greatplainsspca.org/">Great Plains SPCA</a><br>
 							<a href="http://jocartks.org/">JoCART</a><br>
+							<a href="http://www.lisc.org/kansas_city/">Kansas City LISC</a><br>
 							<a href="http://kcpetproject.org/">KC Pet Project</a><br>
-							<a href="https://www.facebook.com/SirabaSchoolInBoiVillage">Siraba School</a><br>
+							<a href="https://www.facebook.com/TheSirabaSchoolVillage">Siraba School</a><br>
 							<a href="http://www.waysidewaifs.org/">Wayside Waifs</a></p>
 						</div>
 					</div>
@@ -187,10 +186,11 @@
 							<a href="http://www.jocodems.org/">Johnson County Democrats</a><br>
 							<a href="http://www.kansasdems.org/">Kansas Dems</a><br>
 							<a href="http://www.lisaforkansas.com">Lisa for Kansas</a><br>
-							<a href="http://www.mcguireforkansas.com">Don McGuire</a><br>
+							<a href="http://www.reggieforcongress.com">Reggie Marselus for U.S. Congress</a><br>
+							<a href="http://www.mcguireforkansas.com">Don McGuire for Kansas House</a><br>
 							Dennis McKinney<br>
 							<a href="http://www.sapphirewire.com">Sapphire Wire</a><br>
-							<a href="http://www.ronworley.com">Ron Worley</a></p>
+							<a href="http://www.ronworley.com">Ron Worley for Kansas House</a></p>
 							<p><strong>Education</strong><br>
 							<a href="http://www.bakeru.edu">Baker University</a><br>
 							<a href="http://www.kcpdc.org">KCPDC</a></p>
