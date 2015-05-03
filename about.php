624bce7abf888347a0d82b07eecb4679a7f76ecc
kylejohnston <kylejohnston@users.noreply.github.com>
2015-05-03 11:55:27 -0500

    Updated bio

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
 				<p>We love design – you can see it in <a href="/#work" title="flow14 - work samples">our work</a> and you’ll love seeing the results of our creative energy come through in your pocketbook. After you’ve had some time to peruse the site and <a href="/#work" title="flow14 - work samples">see a few of the things we’ve created for others</a>, <a href="/contact" title="Contact flow14">contact us</a> and we’ll talk about all of the wonderful things we can create for you.</p>
 				<h2>The Founder.</h2>
 				<img style="float:right; margin:0 0 0 1em;" src="img/headshot.png" class="img-circle" alt="">
-				<p>I'm <strong>Kyle Johnston</strong>, the founder and owner of <span itemprop="name">flow14</span>. When I’m not designing here, I hold down a fantastic day job as the Creative Director at Garmin.</p>
+				<p>I'm <strong>Kyle Johnston</strong>, the founder and owner of <span itemprop="name">flow14</span>. When I’m not designing here, I’m leading the talented creative team at <a href="http://www.degdigital.com">DEG</a>.</p>
 				<p>Here are a few of the other places you can find me on the web:<br>
 					<a class="linkedin" href="http://www.linkedin.com/in/kylejohnston">LinkedIn</a><br>
 					<a class="twitter" href="https://twitter.com/creativecohort">Twitter</a><br>
