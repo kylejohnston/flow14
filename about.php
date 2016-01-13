4dfb39ae9fb7e8f910b4d08ff106524f630f9c50
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-01-12 21:50:34 -0600

    Updated copy

--- about.php
+++ about.php
@@ -14,8 +14,8 @@
 		<div class="row">
 			<div class="span7">
 				<h1>Greetings!</h1>
-				<p><span itemprop="name"><strong>flow14</strong></span> is a cozy little freelance design studio based in <span itemprop="addressLocality">Kansas City</span>. We specialize in helping your small business, non-profit, or political group connect with your audience through engaging websites, logo design, collateral materials and more.</p>
-				<p>We love design – you can see it in <a href="/#work" title="flow14 - work samples">our work</a> and you’ll love seeing the results of our creative energy come through in your pocketbook. After you’ve had some time to peruse the site and <a href="/#work" title="flow14 - work samples">see a few of the things we’ve created for others</a>, <a href="/contact" title="Contact flow14">contact us</a> and we’ll talk about all of the wonderful things we can create for you.</p>
+				<p><span itemprop="name"><strong>flow14</strong></span> is a cozy little design studio based in <span itemprop="addressLocality">Kansas City</span>. We specialize in helping your small business, non-profit, or political group connect with your audience through thoughtfully designed websites, logos, collateral materials and more.</p>
+				<p>We love design – you can see it in <a href="/#work" title="flow14 - work samples">our work</a>. Ready to start your project? <a href="/contact" title="Contact flow14">Contact us</a> &#8211; let&rsquo;s talk about all of the wonderful things we can create for you.</p>
 				<h2>The Founder.</h2>
 				<img style="float:right; margin:0 0 0 1em;" src="img/headshot.png" class="img-circle" alt="">
 				<p>I'm <strong>Kyle Johnston</strong>, the founder and owner of <span itemprop="name">flow14</span>. When I’m not designing here, I hold down a fantastic day job as the Creative Director at Garmin.</p>
