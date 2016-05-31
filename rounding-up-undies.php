f6c9a051df27941b545ebfdb0a701dd10d7af18f
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-05-30 19:02:49 -0500

    Initial commit

---
+++ rounding-up-undies.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Rounding Up Undies | flow14 design works.</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php include('includes/head.php'); ?>
+</head>
+<body>
+<header>
+</header>
+<div class="container">
+	<div class="row">
+		<div class="col-2 center shade">
+				<!--<a href="#">Design Works &#187;</a>
+				<a href="#">About &#187;</a>
+				<a href="#">Contact &#187;</a>-->
+			<h1>Logo</h1>
+			<nav>
+				<a href="#">Design Works &#187;</a>
+				<a href="#">About &times; Contact &#187;</a>
+			</nav>
+		</div>
+		<div class="col-2 hidden-sm">&#160;</div>
+		<div class="col-8 item">
+			<div class="inner">
+				<p class="mono mono-light">Design Works.</p>
+				<h1 class="center"><span>Rounding Up Undies</span></h1>
+				<p><strong>The Story.</strong> Creative Outfitters is a company with a simple message—we love t-shirts. Creative Outfitters’ attitude sets them apart from the competition, and they needed a look that would do the same.</p>
+				<p class="mono"><strong>The Services:</strong> Logo Design</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container item gap">
+	<div class="row">
+		<div class="col-3">
+			<div class="inner">
+				<h3><span class="mono">01.</span>Research &times; Sketches</h3>
+				<p>The logo was designed with a hand-painted font to echo the personal touch Creative Outfitters offers its customers.</p>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="overlay">Research</div>
+			<img src="img/samples/rounding-up-undies/research.jpg" width="100%">
+		</div>
+		<div class="col-3">
+			<div class="overlay overlay-dark">Sketches</div>
+			<img src="img/samples/rounding-up-undies/sketches.jpg" width="100%">
+		</div>
+	</div>
+</div>
+<div class="container item">
+	<div class="row">
+		<div class="push-3 col-6">
+			<div class="inner">
+				<h3><span class="mono">02.</span>Logo Exploration</h3>
+				<p>The logo was designed with a hand-painted font to echo the personal touch Creative Outfitters offers its customers.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<img src="img/samples/rounding-up-undies/logo-exploration.jpg" width="100%">
+		</div>
+	</div>
+</div>
+<div class="container item gap">
+	<div class="row">
+		<div class="flex">
+			<div class="col-4 item-one">
+				<div class="overlay overlay-dark">Round 1</div>
+				<img src="img/samples/rounding-up-undies/logo-options-r1.jpg" width="100%">
+			</div>
+			<div class="col-4 item-two">
+				<div class="inner">
+					<h3><span class="mono">03.</span>Logo Design Options</h3>
+					<p>The logo was designed with a hand-painted font to echo the personal touch Creative Outfitters offers its customers.</p>
+				</div>
+			</div>
+			<div class="col-4 item-three">
+				<div class="overlay overlay-dark">Round 2</div>
+				<img src="img/samples/rounding-up-undies/logo-options-r2.jpg" width="100%">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container item">
+	<div class="row">
+		<div class="col-4">
+			<div class="inner">
+				<h3><span class="mono">04.</span>Final Logo Design</h3>
+				<p>The logo was designed with a hand-painted font to echo the personal touch Creative Outfitters offers its customers.</p>
+			</div>
+		</div>
+		<div class="col-4">
+			<img src="img/samples/rounding-up-undies/final-logo.jpg" width="100%">
+		</div>
+		<div class="col-4">
+			<img src="img/samples/rounding-up-undies/final-logo-guidelines.jpg" width="100%">
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-12 center">
+			<div class="inner">
+				<?php include('includes/cta.php'); ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include('includes/footer.php'); ?>
+
+
+
+</body>
+</html>
