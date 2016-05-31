2cc941a0e9dc33d716b364369220393c3a44cf7d
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-05-30 19:03:17 -0500

    Initial commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,29 +1,29 @@
-		<footer>
-			<div class="container">
-				<div class="row">
-					<div class="span4">
-						<div class="inner">
-							<p>
-								<img src="//flow14.com/img/flow14-logo.png" width="90" height="30" alt=""><br>
-								<a href="/" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Home']);">Home</a> / <a href="/#work" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Work']);">Work</a> / <a href="about" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'About']);">About</a> / <a href="contact" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Contact']);">Contact</a> / <a href="fonts" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Fonts']);">Fonts</a>
-							</p>
-						</div>
-					</div>
-					<div class="span4">
-						<div class="inner" itemscope itemtype="http://schema.org/LocalBusiness">
-							<p><strong>Contact</strong><br>
-							<span itemprop="telephone">913 738-4080</span><br><a href="contact" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'Email']);"><meta itemprop="email" content="contact"><strong>Email</strong></a><br><span itemprop="streetAddress">12725 W 126th St</span>,<br><span itemprop="addressLocality">Overland Park</span>, <span itemprop="addressRegion">KS</span></p>
-						</div>
-					</div>
-					<div class="span4">
-						<div class="inner">
-							<p><strong>Other Projects</strong><br>
-							<a href="http://kylejohnston.tumblr.com/">Tumblr</a><br>
-							<a href="http://blog.flow14.com">Blog (Archive)</a><br>
-							<a href="http://www.singletopicblogofsingletopicblogs.com/">SingleTopicBlogOfSingleTopicBlogs.com</a><br>
-							<a href="iphone-365" onClick="_gaq.push(['_trackEvent', 'Footer', 'Click', 'iPhone 365']);">iPhone 365</a></p>
-						</div>
-					</div>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-4">
+				<div class="inner">
+					<h2>Elsewhere</h2>
+					<p><a href="#">Home</a> / <a href="#">Work</a> / <a href="#">Logo Designs</a> / <a href="#">About</a> / <a href="#">Contact</a> / <a href="#">Fonts</a></p>
 				</div>
 			</div>
-		</footer>
+			<div class="col-4">
+				<div class="inner">
+					<h2>Contact</h2>
+					<p>913 738-4080<br><a href="#">Email</a><br>12725 W 126th St,<br>Overland Park, KS</p>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="inner">
+					<h2>Other Projects</h2>
+					<p>
+					<a href="#">Item One</a><br>
+					<a href="#">Item Two</a><br>
+					<a href="#">Item Three</a><br>
+					<a href="#">Item Four</a><br>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
