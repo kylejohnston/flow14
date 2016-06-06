05f989d8d0966fec00cf4e80090a32b602ba18b8
Kyle Johnston <kjohnston@kyles-iphone.degdigital.com>
2016-06-06 10:27:40 -0500

    Redesign

--- 404.php
+++ 404.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 	<meta charset="utf-8">
-	<title>Page Not Found | flow14 - Design Works.</title>
+	<title>Work Sample: JoCART | Logo Design, Web Presence | flow14</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Page Not Found">
-	<meta name="author" content="Kyle Johnston">
 	<?php include('includes/head.php'); ?>
 </head>
 <body>
-	<?php include('includes/header-nav.php'); ?>
-	<div class="container items">
-		<div class="row">
-			<div class="span7">
-				<h1>Oops.</h1>
-				<p>Sorry, the page you’re looking for is has moved or is no longer available.</p>
-				<p>Go <a href="http://www.flow14.com">back to the homepage</a>, check out <a href="http://www.flow14.com/work">recent work</a>, or <a href="http://www.flow14.com/contact">get in touch</a> with any questions.</p>
+<div class="container">
+	<div class="row">
+		<?php include('includes/header-nav.php'); ?>
+		<div class="col-8 item">
+			<div class="inner">
+				<p class="mono mono-light">404</p>
+				<h1 class="center"><span>Oops</span></h1>
+				<p>Sorry, the page you’re looking for is has moved or is no longer available. What&rsquo;s next? Go <a href="http://www.flow14.com">back to the homepage</a>, check out <a href="http://www.flow14.com/work">recent work</a>, or <a href="http://www.flow14.com/contact">get in touch</a> with any questions.</p>
+				<p class="mono mono-light">&therefore;</p>
 			</div>
 		</div>
 	</div>
-	<div class="container marketing">
-		<?php include('includes/footer.php'); ?>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-12 center">
+			<div class="inner">
+				<?php include('includes/cta.php'); ?>
+			</div>
+		</div>
 	</div>
-	<?php include('includes/foot.php'); ?>
+</div>
+
+<?php include('includes/footer.php'); ?>
+
+
+
 </body>
 </html>
