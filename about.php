849c9553f0ba78685d9ccf4dbf078997f4b88160
Kyle Johnston <kjohnston@kyles-iphone.degdigital.com>
2016-06-07 08:46:44 -0500

    Working on new layout

--- about.php
+++ about.php
@@ -1,41 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 	<meta charset="utf-8">
-	<title>About flow14 - Design Works.</title>
+	<title>About &times; Contact flow14 - Design Works.</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Learn more about flow14, a design studio that specializes in helping small businesses, non-profits, and political groups with logo design, website design and more">
-	<meta name="author" content="Kyle Johnston">
 	<?php include('includes/head.php'); ?>
 </head>
-<body>
-	<?php include('includes/header-nav.php'); ?>
-	<div class="container items">
-		<div class="row">
-			<div class="span7">
-				<h1>Greetings!</h1>
-				<p><span itemprop="name"><strong>flow14</strong></span> is a cozy little design studio based in <span itemprop="addressLocality">Kansas City</span>. We specialize in helping your small business, non-profit, or political group connect with your audience through thoughtfully designed websites, logos, collateral materials and more.</p>
-				<p>We love design – you can see it in <a href="/#work" title="flow14 - work samples">our work</a>. Ready to start your project? <a href="/contact" title="Contact flow14">Contact us</a> &#8211; let&rsquo;s talk about all of the wonderful things we can create for you.</p>
+<body class="cool">
+<div class="container">
+	<div class="row">
+		<?php include('includes/header-nav.php'); ?>
+		<div class="col-8 item">
+			<div class="inner">
+				<p class="mono mono-light">Greetings!</p>
+				<h1 class="center"><span>About &times; Contact</span></h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container item">
+	<div class="row">
+		<div class="push-2 col-8">
+			<div class="inner">
+				<h2>Greetings!</h2>
+				<p>flow14 is a cozy little design studio based in Kansas City. We specialize in helping your small business, non-profit, or political group connect with your audience through thoughtfully designed websites, logos, collateral materials and more.</p>
+				<p>We love design &#8211; you can see it in our work. Ready to start your project? Contact us &#8211; let&rsquo;s talk about all of the wonderful things we can create for you.</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-6">
+			<div class="inner">
+				<h2>Contact.</h2>
+				<p>Blah. Blah. Blah.</p>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="inner">
 				<h2>The Founder.</h2>
-				<img style="float:right; margin:0 0 0 1em;" src="img/headshot.png" class="img-circle" alt="">
-				<p>I'm <strong>Kyle Johnston</strong>, the founder and owner of <span itemprop="name">flow14</span>. When I’m not designing here, I hold down a fantastic day job as the Creative Director at Garmin.</p>
+				<p><strong>IMAGE</strong></p>
+				<p>I&rsquo;m Kyle Johnston, the founder and owner of flow14. When I&rsquo;m not designing here, I hold down a fantastic day job as the Creative Director at DEG.</p>
 				<p>Here are a few of the other places you can find me on the web:<br>
-					<a class="linkedin" href="http://www.linkedin.com/in/kylejohnston">LinkedIn</a><br>
-					<a class="twitter" href="https://twitter.com/creativecohort">Twitter</a><br>
-					<a class="instagram" href="https://instagram.com/kylejohnston/">Instagram</a><br>
-					<a class="dribbble" href="https://dribbble.com/kylejohnston">dribbble</a><br>
-					<a class="github" href="https://github.com/kylejohnston">GitHub</a><br>
-					<a class="behance" href="https://www.behance.net/KyleJohnston">Behance</a><br>
-					<a class="flickr" href="http://www.flickr.com/photos/flow14/">Flickr</a><br>
-					<a class="vsco" href="http://kylejohnston.vsco.co/">VSCO</a><br>
-					<a class="vimeo" href="http://vimeo.com/flow14">Vimeo</a><br>
+					<a class="linkedin" href="http://www.linkedin.com/in/kylejohnston">LinkedIn</a>, 
+					<a class="twitter" href="https://twitter.com/creativecohort">Twitter</a>, 
+					<a class="instagram" href="https://instagram.com/kylejohnston/">Instagram</a>, 
+					<a class="dribbble" href="https://dribbble.com/kylejohnston">dribbble</a>, 
+					<a class="github" href="https://github.com/kylejohnston">GitHub</a>, 
+					<a class="behance" href="https://www.behance.net/KyleJohnston">Behance</a>, 
+					<a class="flickr" href="http://www.flickr.com/photos/flow14/">Flickr</a>, 
+					<a class="vsco" href="http://kylejohnston.vsco.co/">VSCO</a>, 
+					<a class="vimeo" href="http://vimeo.com/flow14">Vimeo</a>
 				</p>
 			</div>
 		</div>
 	</div>
-	<div class="container marketing">
-		<?php include('includes/footer.php'); ?>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-12 center">
+			<div class="inner">
+				<?php include('includes/cta.php'); ?>
+			</div>
+		</div>
 	</div>
-	<?php include('includes/foot.php'); ?>
+</div>
+
+<?php include('includes/footer.php'); ?>
+
+
+
 </body>
 </html>
