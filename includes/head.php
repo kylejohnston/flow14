cbc74103f4db4cd1da68114671c38792f2bb704a
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-06-07 21:28:55 -0500

    Moved rel to end of element
    via https://github.com/joshbuchea/HEAD

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,5 @@
 	<!-- Le styles -->
-	<link rel="stylesheet" href="css/stylesheet.css">
+	<link href="css/stylesheet.css" rel="stylesheet">
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
 		<script src="js/html5shiv.js"></script>
