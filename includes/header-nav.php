d610ea2662ab0344d4c5ab125e3cdfabd94ef252
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-06-07 22:34:31 -0500

    Prepped for SVG-based logo

--- includes/header-nav.php
+++ includes/header-nav.php
@@ -1,5 +1,5 @@
 		<header class="col-2 center shade">
-			<h1>flow14</h1>
+			<h1 class="logo"><a href="http://www.flow14.com">flow14</a></h1>
 			<nav>
 				<a href="#">Design Works &#187;</a>
 				<a href="#">About &times; Contact &#187;</a>
