594b2f6645d38bcb1249833a063ba758802662fa
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-07-12 22:17:26 -0500

    Removed comments

--- includes/main-nav.php
+++ includes/main-nav.php
@@ -1,7 +1,4 @@
 		<div class="col-2 center shade">
-				<!--<a href="#">Design Works &#187;</a>
-				<a href="#">About &#187;</a>
-				<a href="#">Contact &#187;</a>-->
 			<h1>Logo</h1>
 			<nav>
 				<a href="#">Design Works &#187;</a>
