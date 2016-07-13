17fefe20bfc9e1fff593f988187e593bafe85695
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-07-12 22:16:51 -0500

    Initial commit

--- includes/cta_web.php
+++ includes/cta_web.php
@@ -1,4 +1,4 @@
 				<p class="mono mono-light">&because;</p>
-				<h2>Need a design for your non-profit?</h2>
+				<h2>Do you need a fresh, new website design?</h2>
 				<p>Contact me for an estimate.</p>
 				<p><a class="ghost-button" href="about.php#contact">Let&rsquo; talk.</a></p>
