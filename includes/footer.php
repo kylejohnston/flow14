5dac816c271cbd9a54a8057f6058e2a0cc4f9b53
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-07-12 22:17:04 -0500

    Updated links

--- includes/footer.php
+++ includes/footer.php
@@ -4,23 +4,22 @@
 			<div class="col-4">
 				<div class="inner">
 					<h2>Elsewhere</h2>
-					<p><a href="#">Home</a> / <a href="#">Work</a> / <a href="#">Logo Designs</a> / <a href="#">About</a> / <a href="#">Contact</a> / <a href="#">Fonts</a></p>
+					<p><a href="/">Home</a> / <a href="/">Work</a><!--  / <a href="#">Logo Designs</a> --> / <a href="about">About</a> / <a href="about#contact">Contact</a> / <a href="fonts">Fonts</a></p>
 				</div>
 			</div>
 			<div class="col-4">
 				<div class="inner">
 					<h2>Contact</h2>
-					<p>913 738-4080<br><a href="#">Email</a><br>12725 W 126th St,<br>Overland Park, KS</p>
+					<p>913 738-4080<br><a href="about#contact">Email</a><br>12725 W 126th St,<br>Overland Park, KS</p>
 				</div>
 			</div>
 			<div class="col-4">
 				<div class="inner">
 					<h2>Other Projects</h2>
 					<p>
-					<a href="#">Item One</a><br>
-					<a href="#">Item Two</a><br>
-					<a href="#">Item Three</a><br>
-					<a href="#">Item Four</a><br>
+					<a href="http://blog.flow14.com">Blog (Archive)</a><br>
+					<a href="http://www.singletopicblogofsingletopicblogs.com/">SingleTopicBlogOfSingleTopicBlogs.com</a><br>
+					<a href="http://www.flow14.com/iphone-365-project">365 Project</a><br>
 					</p>
 				</div>
 			</div>
@@ -34,5 +33,4 @@
 	</div>
 </footer>
 
-<script src="js/lazysizes.min.js" async=""></script>
-<script src="js/fittext.js"></script>
+<script src="js/flow14.min.js"></script>
