140d9f2ec003e05f9f1a4f7363e5abf915703d05
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-07-12 22:17:16 -0500

    Link to minimized stylesheet

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,5 @@
 	<!-- Le styles -->
-	<link href="css/stylesheet.css" rel="stylesheet">
+	<link href="css/stylesheet.min.css" rel="stylesheet">
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
 		<script src="js/html5shiv.js"></script>
