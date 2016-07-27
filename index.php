f5a5b1220bbd8a22399b82ee2a53d753715f1d8a
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-4.local>
2016-07-26 20:31:19 -0500

    Added ALT tags to images

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-6">
-			<a href="rounding-up-undies"><img src="img/samples/rounding-up-undies/thumbnail.2x.jpg" width="100%" height="auto"></a>
+			<a href="rounding-up-undies"><img src="img/samples/rounding-up-undies/thumbnail.2x.jpg" width="100%" height="auto" alt="Rounding Up Undies logo design"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="rounding-up-undies">Rounding Up Undies</a></h2>
@@ -35,7 +35,7 @@
 			</div>
 		</div>
 		<div class="col-6">
-			<a href="creative-outfitters"><img class="grad" src="img/samples/creative-outfitters/thumbnail.png" width="100%" height="auto"></a>
+			<a href="creative-outfitters"><img class="grad" src="img/samples/creative-outfitters/thumbnail.png" width="100%" height="auto" alt="Creative Outfitters logo design"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="creative-outfitters">Creative Outfitters</a></h2>
@@ -49,7 +49,7 @@
 	</div><!-- /row -->
 	<div class="row">
 		<div class="col-6">
-			<a href="lisa-for-kansas"><img class="gradrev" src="img/samples/lisa-for-kansas/thumbnail.2x.jpg" width="100%" height="auto" class="lazyload"></a>
+			<a href="lisa-for-kansas"><img class="gradrev" src="img/samples/lisa-for-kansas/thumbnail.2x.jpg" width="100%" height="auto" class="lazyload" alt="Lisa Johnston for US Senate design system"></a>
 			<a href="lisa-for-kansas"<img src="img/samples/lisa-for-kansas/thumbnail.2x.jpg" width="100%"></a>
 			<div class="sample">
 				<div class="sample-desc">
@@ -62,7 +62,7 @@
 			</div>
 		</div>
 		<div class="col-6">
-			<a href="kc-lisc"><img src="img/samples/kc-lisc/thumbnail.jpg" width="100%" height="auto" class="lazyload"></a>
+			<a href="kc-lisc"><img src="img/samples/kc-lisc/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Kansas City LISC infographic design"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="kc-lisc">KC Lisc</a></h2>
@@ -76,7 +76,7 @@
 	</div><!-- /row -->
 	<div class="row">
 		<div class="col-6">
-			<a href="kansas-dems"><img src="img/samples/kansas-dems/thumbnail.jpg" width="100%" height="auto" class="lazyload"></a>
+			<a href="kansas-dems"><img src="img/samples/kansas-dems/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Kansas Dems logo design"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="kansas-dems">Kansas Dems</a></h2>
@@ -88,7 +88,7 @@
 			</div>
 		</div>
 		<div class="col-6">
-			<a href="websites"><img class="grad" src="img/samples/websites/thumbnail.png" width="100%" height="auto" class="lazyload"></a>
+			<a href="websites"><img class="grad" src="img/samples/websites/thumbnail.png" width="100%" height="auto" class="lazyload" alt="Responsive website designs"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="websites">Responsive Websites</a></h2>
@@ -102,7 +102,7 @@
 	</div><!-- /row -->
 	<div class="row">
 		<div class="col-6">
-			<a href="lisa-for-kansas-senate"><img class="gradrev" src="img/samples/lisa-for-kansas-senate/thumbnail.2x.png" width="100%" height="auto" class="lazyload"></a>
+			<a href="lisa-for-kansas-senate"><img class="gradrev" src="img/samples/lisa-for-kansas-senate/thumbnail.2x.png" width="100%" height="auto" class="lazyload" alt="Lisa Johnston for Kansas Senate design system"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="lisa-for-kansas-senate">Lisa for Kansas Senate</a></h2>
@@ -114,7 +114,7 @@
 			</div>
 		</div>
 		<div class="col-6">
-			<a href="harmony-hearing-aids"><img class="gradrev" src="img/samples/harmony/thumbnail.png" width="100%" height="auto" class="lazyload"></a>
+			<a href="harmony-hearing-aids"><img class="gradrev" src="img/samples/harmony/thumbnail.png" width="100%" height="auto" class="lazyload" alt="Harmony Hearing Aids logo design"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="harmony-hearing-aids">Harmony</a></h2>
@@ -129,7 +129,7 @@
 	<div class="row">
 		<div class="inner center"><h2>&times; Other works &times;</h2></div>
 		<div class="col-4">
-			<a href="http://www.kylewjohnston.com"><img src="img/samples/video.jpg" width="100%" height="auto" class="lazyload"></a>
+			<a href="http://www.kylewjohnston.com"><img src="img/samples/video.jpg" width="100%" height="auto" class="lazyload" alt="Kyle Johnston advertising portfolio"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="http://www.kylewjohnston.com">Advertising</a></h2>
@@ -141,7 +141,7 @@
 			</div>
 		</div>
 		<div class="col-4">
-			<a href="iphone-365-project"><img src="img/samples/365.jpg" width="100%" height="auto" class="lazyload"></a>
+			<a href="iphone-365-project"><img src="img/samples/365.jpg" width="100%" height="auto" class="lazyload" alt="365 project"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="iphone-365-project">365 Project</a></h2>
@@ -153,7 +153,7 @@
 			</div>
 		</div>
 		<div class="col-4">
-			<a href="fonts"><img src="img/fonts/thumbnail.jpg" width="100%" height="auto" class="lazyload"></a>
+			<a href="fonts"><img src="img/fonts/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Free fonts"></a>
 			<div class="sample">
 				<div class="sample-desc">
 					<h2><a href="fonts">Fonts</a></h2>
