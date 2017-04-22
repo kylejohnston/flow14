f4ead4d9f111e69a8f2ee59fc20e34c33070a775
Kyle <kylejohnston@users.noreply.github.com>
2017-04-22 11:48:25 -0500

    Initial commit

---
+++ logos.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Work Sample: Logo Design | flow14</title>
+	<meta name="description" content="Logo designs for small businesses, non-profits, and political groups" />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/logos" rel="canonical" />
+	<?php include('includes/head.php'); ?>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<?php include('includes/header-nav.php'); ?>
+		<div class="col-8 item">
+			<div class="inner">
+				<p class="mono mono-light">Design Works.</p>
+				<h1 id="fittext" class="center">Logo Designs</h1>
+				<p class="center">A selection of logos designed for small businesses, non-profits and political groups.</p>
+				<p class="mono mono-light">&therefore;</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container item">
+	<div class="inner center"><h2>&times; small business logo design &times;</h2></div>
+	<div class="row">
+		<div class="col-6">
+			<a href="creative-outfitters"><img src="img/samples/logos/1440x810/creative-outfitters-2x.png" width="100%" height="auto" alt="Creative Outfitters logo"></a>
+			<div class="inner center">
+				<p><a class="ghost-button" href="creative-outfitters">Read the case study</a></p>
+			</div>
+		</div>
+		<div class="col-6">
+			<a href="harmony-hearing-aids"><img src="img/samples/logos/1440x810/harmony-hearing-aids-2x.png" width="100%" height="auto" alt="Harmony Hearing Aids logo"></a>
+			<div class="inner center">
+				<p class="center"><a class="ghost-button" href="harmony-hearing-aids">Read the case study</a></p>
+			</div>
+		</div>
+	</div><!-- /row -->
+	<div class="row">
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/campaign-and-win-2x.png" width="100%" height="auto" alt="Campaign And Win logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/taylor-excavating-2x.png" width="100%" height="auto" alt="Taylor Excavating logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+	</div><!-- /row -->
+	<div class="row">
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/shawnas-sweets-2x.png" width="100%" height="auto" alt="Shawna's Sweets logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/ypo-kc-2x.png" width="100%" height="auto" alt="YPO KC logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+	</div><!-- /row -->
+	<div class="row">
+		<div class="col-3">&#160;</div>
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/sapphirewire-2x.png" width="100%" height="auto" alt="SapphireWire logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+		<div class="col-3">&#160;</div>
+	</div><!-- /row -->
+
+	<div class="inner center"><h2>&times; non-profit logo design &times;</h2></div>
+	<div class="row">
+		<div class="col-6">
+			<a href="rounding-up-undies"><img src="img/samples/logos/1440x810/rounding-up-undies-2x.png" width="100%" height="auto" alt="Rounding Up Undies logo"></a>
+			<div class="inner center">
+				<p><a class="ghost-button" href="rounding-up-undies">Read the case study</a></p>
+			</div>
+		</div>
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/kapc-2x.png" width="100%" height="auto" alt="KAPC logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+	</div><!-- /row -->
+	<div class="row">
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/mission-cup-2x.png" width="100%" height="auto" alt="Mission Cup logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/kc-metro-art-2x.png" width="100%" height="auto" alt="KC Metro Animal Response Team logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+	</div><!-- /row -->
+
+	<div class="inner center"><h2>&times; political logo design &times;</h2></div>
+	<div class="row">
+		<div class="col-6">
+			<a href="lisa-for-kansas"><img src="img/samples/logos/1440x810/lisa-us-senate-2x.png" width="100%" height="auto" alt="Lisa Johnston for US Senate logo"></a>
+			<div class="inner center">
+				<p><a class="ghost-button" href="lisa-for-kansas">Read the case study</a></p>
+			</div>
+		</div>
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/reggie-for-congress-2x.png" width="100%" height="auto" alt="Reggie Marselus for US Congress logo">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+	</div><!-- /row -->
+	<div class="row">
+		<div class="col-6">
+			<a href="lisa-for-kansas-senate"><img src="img/samples/logos/1440x810/lisa-kansas-senate-2x.png" width="100%" height="auto" alt="Lisa Johnston for Kansas Senate"></a>
+			<div class="inner center">
+				<p><a class="ghost-button" href="lisa-for-kansas-senate">Read the case study</a></p>
+			</div>
+		</div>
+		<div class="col-6">
+			<a href="kansas-dems"><img src="img/samples/logos/1440x810/kansas-dems-2x.png" width="100%" height="auto" alt="Kansas Dems logo"></a>
+			<div class="inner center">
+				<p><a class="ghost-button" href="kansas-dems">Read the case study</a></p>
+			</div>
+		</div>
+	</div><!-- /row -->
+	<div class="row">
+		<div class="col-3">&#160;</div>
+		<div class="col-6">
+			<img src="img/samples/logos/1440x810/mcguire-house-2x.png" width="100%" height="auto" alt="Don McGuire for Kansas House">
+			<div class="inner center">
+				<p>&#160;</p>
+			</div>
+		</div>
+		<div class="col-3">&#160;</div>
+	</div><!-- /row -->
+
+</div>
+
+<?php include('includes/footer.php'); ?>
+
+
+<script type="text/javascript">
+	window.fitText(document.getElementById("fittext"), .75)
+</script>
+
+</body>
+</html>
