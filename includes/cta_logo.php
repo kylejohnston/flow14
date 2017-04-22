5dc1a9915e0b57122e7b3e45545c4adc6f8a3b90
Kyle <kylejohnston@users.noreply.github.com>
2017-04-22 18:30:00 -0500

    Initial commit

---
+++ includes/cta_logo.php
@@ -0,0 +1,4 @@
+				<p class="mono mono-light">&because;</p>
+				<h2>Do you need a standout logo design?</h2>
+				<p>Contact me for an estimate today.</p>
+				<p><a class="ghost-button" href="about.php#contact">Let&rsquo; talk.</a></p>
