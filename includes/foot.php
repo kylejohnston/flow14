5b97b84642e009fd006932d9b7d622ca35092be9
Kyle <kylejohnston@users.noreply.github.com>
2017-04-24 09:41:02 -0500

    Added HubSpot code

--- includes/foot.php
+++ includes/foot.php
@@ -3,8 +3,11 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="//flow14.com/js/jquery.js">\x3C/script>')</script>
 	<script src="//flow14.com/js/jquery.flexslider-min.js"></script>
-	
-	<!-- statcounter analytics -->
+
+<!-- HubSpot -->
+	<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/3292081.js"></script>
+
+<!-- statcounter analytics -->
 	<script type="text/javascript">
 		var sc_project=651567; 
 		var sc_invisible=1; 
@@ -19,12 +22,12 @@
 	alt="statistics for vBulletin"></a></div></noscript>
 
 <!-- Pinterest -->
-<script type="text/javascript">
-(function(d){
-  var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
-  p.type = 'text/javascript';
-  p.async = true;
-  p.src = '//assets.pinterest.com/js/pinit.js';
-  f.parentNode.insertBefore(p, f);
-}(document));
-</script>
+	<script type="text/javascript">
+	(function(d){
+	  var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+	  p.type = 'text/javascript';
+	  p.async = true;
+	  p.src = '//assets.pinterest.com/js/pinit.js';
+	  f.parentNode.insertBefore(p, f);
+	}(document));
+	</script>
