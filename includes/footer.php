920e571b213704449d34c73a7fbd622795ae8b0e
kylejohnston <kylejohnston@users.noreply.github.com>
2017-08-03 22:59:04 -0500

    Added hotjar script

--- includes/footer.php
+++ includes/footer.php
@@ -33,17 +33,14 @@
 </footer>
 
 <script src="js/flow14.min.js"></script>
-<!-- statcounter analytics -->
-<script type="text/javascript">
-	var sc_project=651567; 
-	var sc_invisible=1; 
-	var sc_security="59987d53"; 
+<!-- Hotjar Tracking Code for http://www.flow14.com -->
+<script>
+	(function(h,o,t,j,a,r){
+		h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		h._hjSettings={hjid:583449,hjsv:5};
+		a=o.getElementsByTagName('head')[0];
+		r=o.createElement('script');r.async=1;
+		r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		a.appendChild(r);
+	})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
-<script type="text/javascript"
-src="http://www.statcounter.com/counter/counter.js"></script>
-<noscript><div class="statcounter"><a title="statistics for
-vBulletin" href="http://statcounter.com/vbulletin/"
-target="_blank"><img class="statcounter"
-src="http://c.statcounter.com/651567/0/59987d53/1/"
-alt="statistics for vBulletin"></a></div></noscript>
-
