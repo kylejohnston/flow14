1510cc68d239ef6f36592c1d3cd78ffa2dfd2a1b
Kyle Johnston <flow14@gmail.com>
2018-06-20 16:28:14 -0500

    Initial commit

---
+++ hnc-living-foundation.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HNC Living Foundation | flow14 design works.</title>
+	<meta name="description" content="Website redesign for HNC Living Foundation, a nonprofit that partners with nonprofit healthcare providers and organizations to fund grants that help head and neck cancer patients live life fully." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/rounding-up-undies" rel="canonical" />
+	<?php include('includes/head.php'); ?>
+	<style type="text/css">
+	.item { margin-top:2em; padding-bottom:2em; }
+	body.neutral{background-color:#f8f8f8;background-image:linear-gradient(#f8f8f8, #d1d1d1);}
+	</style>
+</head>
+<body class="neutral">
+<header>
+</header>
+<div class="container">
+	<?php include('includes/header-nav--small.php'); ?>
+	<div class="row">
+		<div class="col-2 hidden-sm">&#160;</div>
+		<div class="col-8">
+			<div class="inner">
+				<h1 id="fittext" class="center">HNC Living Foundation</h1>
+				<p>What do you do when your nonprofit has successfully helped more than 1,200 head and neck cancer patients with their treatment and recovery, but you’re ready to expand beyond your local roots?</p>
+				<p>For <a href="https://www.hncliving.org">HNC Living Foundation</a>&#8212;a nonprofit who partners with healthcare providers to fund grants that help head and neck cancer patients&#8212;the answer was a modern website designed to attract more donors and find new partners.</p>
+				<p class="mono mono-light">&therefore;</p>
+			</div>
+		</div>
+		<div class="col-2 hidden-sm">&#160;</div>
+	</div>
+</div>
+
+
+<div class="container item">
+	<div class="row">
+		<div class="col-1 hidden-sm">&#160;</div>
+		<div class="col-5">
+			<div class="inner">
+				<h2>The Challenge</h2>
+				<p>HNC Living, a Kansas City-based nonprofit founded in 2015, has successfully helped more than 1,200 head and neck cancer patients live life fully by funding more than $555,000 in grants for treatment, therapy, and support services.</p>
+				<p>However, their website didn’t effectively communicate their mission, wasn’t driving donations, and wasn’t easy to navigate.</p>
+				<p>HNC Living asked flow14 to reimagine their website and give them an effective platform to increase donations, reach more partners and patients, and grow nationally.</p>
+			</div>
+		</div>
+		<div class="col-5">
+			<div class="inner">
+				<img data-src="img/samples/hnc-living/old-homepage__700.jpg" alt="old homepage" class="lazyload" width="100%">
+				<p class="mono">The old website</p>
+			</div>
+		</div>
+		<div class="col-1 hidden-sm">&#160;</div>
+	</div>
+</div>
+
+<!-- VISION -->
+<div class="container item">
+	<div class="row">
+		<div class="col-3 hidden-sm">&#160;</div>
+		<div class="col-6">
+			<div class="inner">
+				<h2>Process: Project Vision</h2>
+				<p>For the kickoff meeting, I developed a project vision to help drive a conversation about HNC Living’s audiences and goals. Based on my experience using their site, I shared  ideas for improving the organization, calls to action, and more.</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<img data-src="img/samples/hnc-living/wireframes__1400.jpg" alt="wireframes" class="lazyload" width="100%">
+		</div>
+	</div>
+</div>
+
+<!-- RESEARCH AND AUDITING -->
+<div class="container item">
+	<div class="row">
+		<div class="col-3 hidden-sm">&#160;</div>
+		<div class="col-6">
+			<div class="inner">
+				<h2>RESEARCH &times; AUDITING</h2>
+				<p>I researched scores of nonprofits in cancer care and other areas to understand different models for site organization, content, and calls to action. This research, informed by HNC Living’s goals and audiences, led to a new navigation structure that directly addressed their key audiences: donors, healthcare providers, and patients.</p>
+				<p>Next, a SEO audit underscored the benefits of this approach, and informed a content strategy that spoke more clearly to their audiences and allowed for hyper-relevant calls to action.</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-6">
+			<div class="inner">
+				<img data-src="img/samples/hnc-living/research__700.jpg" alt="research" class="lazyload" width="100%">
+				<p class="mono">Researching similar nonprofits</p>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="inner">
+				<img data-src="img/samples/hnc-living/ia-audit__700.jpg" alt="seo and information architecture audit" class="lazyload" width="100%">
+				<p class="mono">Information architecture audit</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- DESIGN ELEMENTS -->
+<!-- RESEARCH AND AUDITING -->
+<div class="container item">
+	<div class="row">
+		<div class="col-6">
+			<div class="inner">
+				<h2>Copywriting &times; Content Strategy</h2>
+				<p>For the all-important website content, I created a collaborative document that allowed project stakeholders to provide real-time suggestions and edits.</p>
+				<p>Informed by the SEO audit and HNC Living's input, I crafted page titles, descriptions, and page content designed to draw in more traffic, clearly communicate HNC Living's purpose, and drive visitors to act.</p>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="inner">
+				<img data-src="img/samples/hnc-living/copywriting__700.jpg" alt="content and copywriting" class="lazyload" width="100%">
+				<p class="mono">Collaborative copywriting</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE COMPS -->
+<div class="container item">
+	<div class="row">
+		<div class="col-3 hidden-sm">&#160;</div>
+		<div class="col-6">
+			<div class="inner">
+				<h2>Page Comps</h2>
+				<p>As the content developed, I created sample pages that allowed the HNC Living team to understand how their new website could look and provide feedback early in the process.</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<img data-src="img/samples/hnc-living/high-fidelity-page-comp__1400.jpg" alt="high fidelity page comps" class="lazyload" width="100%">
+		</div>
+	</div>
+</div>
+
+<!-- HOMEPAGE -->
+<div class="container item">
+	<div class="row">
+		<div class="col-3 hidden-sm">&#160;</div>
+		<div class="col-6">
+			<div class="inner">
+				<h2>The New Homepage</h2>
+				<p>The new <a href="https://www.hncliving.org">HNCLiving.org</a> launched in April 2018. Their new homepage that clearly communicates their mission, the audiences they serve, and how they’ve helped scores of head and neck cancer patients live life fully, during and after treatment.</p>
+			</div>
+			<img data-src="img/samples/hnc-living/homepage__960.jpg" alt="new homepage design" class="lazyload" width="100%">
+		</div>
+	</div>
+</div>
+
+<!-- CATEGORY PAGES -->
+<div class="container item">
+	<div class="row">
+		<div class="col-3 hidden-sm">&#160;</div>
+		<div class="col-6">
+			<div class="inner">
+				<h2>Clear calls to action</h2>
+				<p>New, audience-centric landing pages speak directly to HNC Living’s key audiences, ensuring they clearly understand what the organization has to offer with clear calls to action.</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-6">
+			<div class="inner">
+				<img data-src="img/samples/hnc-living/for-providers__700.jpg" alt="landing page: for healthcare providers" class="lazyload" width="100%">
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="inner">
+				<img data-src="img/samples/hnc-living/for-patients__700.jpg" alt="landing page: for patients" class="lazyload" width="100%">
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- MANUAL -->
+<div class="container item">
+	<div class="row"><div class="flex">
+		<div class="col-6 item-one">
+			<div class="inner">
+				<img data-src="img/samples/hnc-living/website-manual__700.jpg" alt="website manual" class="lazyload" width="100%">
+			</div>
+		</div>
+		<div class="col-6 item-two">
+			<div class="inner">
+				<h2>WEBSITE MANUAL</h2>
+				<p>To ensure the website continues to evolve and serve the HNC Living’s needs well into the future, I created a comprehensive manual that details how to update and maintain the website.</p>
+			</div>
+		</div>
+	</div></div>
+</div>
+
+<div class="container item">
+	<div class="row">
+		<div class="col-3 hidden-sm center">&#160;</div>
+		<div class="col-6">
+		<h2>Results</h2>
+			<p class="inner">In the first full month after launching the website, <a href="https://www.hncliving.org">HNCLiving.org</a> saw a:</p>
+			<h1 id="fittexttwo">194%</h1>
+			<p class="mono">increase in visits to the donation page</p>
+			<h1 id="fittexttre">30%</h1>
+			<p class="mono">increase in overall pageviews</p>
+		</div>
+		<div class="col-3 hidden-sm">&#160;</div>
+	</div><!-- /row -->
+</div>
+
+<div class="container item">
+	<div class="row">
+		<div class="col-3 hidden-sm center">&#160;</div>
+		<div class="col-6">
+		<h2>&horbar; &rdquo; &horbar;<br>client testimonial</h2>
+			<p class="inner">&ldquo;Kyle did an excellent job of laying our the opportunities for improving our website. He clearly identified objectives and planned an approach we could follow and participate in that didn&rsquo;t disrupt our normal activities. Our finished product came in on-time, on-budget, and on-target. We will most definitely continue to rely on Kyle and flow14 to keep our web presence current and designed to enhance our brand and identity.&rdquo;<br><em>&mdash;Tom Walsh, HNC Living Foundation Co-Founder</em></p>
+		</div>
+	</div><!-- /row -->
+</div>
+
+
+
+
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-12 center">
+			<div class="inner">
+				<?php include('includes/cta_np.php'); ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include('includes/footer.php'); ?>
+
+<script type="text/javascript">
+	window.fitText(document.getElementById("fittext"), .65)
+	window.fitText(document.getElementById("fittexttwo"), .65)
+	window.fitText(document.getElementById("fittexttre"), .65)
+</script>
+
+
+
+</body>
+</html>
