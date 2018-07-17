d4aa4ba85c897a0ab8d1498b124cd1124f14ddb3
Kyle Johnston <flow14@gmail.com>
2018-07-17 11:50:41 -0500

    Updated CTAs

--- includes/cta_web.php
+++ includes/cta_web.php
@@ -1,4 +1,4 @@
 				<p class="mono">&because;</p>
 				<h2>Do you need a fresh, new website design?</h2>
 				<p>Contact me for an estimate.</p>
-				<p><a class="ghost-button" href="about.php#contact">Let&rsquo; talk.</a></p>
+				<p><a class="ghost-button" href="about.php#contact">Let&rsquo;s talk.</a></p>
