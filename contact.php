e7ec8decd6281e027d37edeaa888150887c8f0a9
Kyle Johnston <flow14@gmail.com>
2018-08-20 11:54:04 -0500

    Initial commit

---
+++ contact.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contact flow14 - Design Works.</title>
+	<meta name="description" content="A graphic design studio that specializes in helping small businesses, nonprofits, and political groups with logo design, website design and more." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/about" rel="canonical" />
+	<?php include('includes/head.php'); ?>
+	<style>
+	@media (min-width: 1012px) {
+	.inner-1{padding:0 4px}.inner-2{padding:0 8px}.inner-3{padding:0 16px}.inner-4{padding:0 24px}.inner-5{padding:0 32px}.inner-6{padding:0 40px}
+	}
+	</style>
+</head>
+<body class="neutral">
+	<?php include('includes/header-nav.php'); ?>
+	<article class="content">
+		<section class="block">
+			<div class="pattern__2-8-2">
+				<div class="inner">
+					<h1 id="fittext" class="center">Say hello!</h1>
+				</div>
+			</div>
+		</section>
+
+		<section class="block">
+			<div class="pattern__2-8-2">
+				<div class="inner">
+					<div class="inner">
+						<a name="contact"></a>
+						<section class="block">
+							<div class="col-8">
+								<h2>Email</h2>
+								<p>For new business inquiries, please contact <script type="text/javascript">
+								//<![CDATA[
+								<!--
+								var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+								"=93){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
+								".substr(0,ol);}f(\")401,\\\"v|pckv&iy),!ot610\\\\IRP]z?#\\\"\\\\($uh520\\\\" +
+								"j330\\\\{ (7+5`530\\\\bPSX420\\\\r\\\\t\\\\@YYR320\\\\_^BI610\\\\HKJYZMJ600" +
+								"\\\\d130\\\\WADJ}kn#vuz6#'b{771\\\\tQ771\\\\cbhd1e}dngh&_?ge410\\\\520\\\\\\"+
+								"\\\\\\230\\\\F[P130\\\\230\\\\020\\\\000\\\\230\\\\000\\\\600\\\\^330\\\\00" +
+								"0\\\\010\\\\100\\\\630\\\\t\\\\600\\\\410\\\\\\\"(f};o nruter};))++y(^)i(tA" +
+								"edoCrahc.x(edoCrahCmorf.gnirtS=+o;721=%y;i=+y)401==i(fi{)++i;l<i;0=i(rof;ht" +
+								"gnel.x=l,\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"                                ;
+								while(x=eval(x));
+								//-->
+								//]]>
+								</script></p>
+								<h2>Phone</h2>
+								<p>913-738-4080</p>
+								<h2>Location</h2>
+								<p>Overland Park, KS AKA the Kansas City Metro Area</p>
+							</div>
+							<div class="col-4">
+								<h3>Elsewhere</h3>
+								<p>
+									<a class="linkedin" href="http://www.linkedin.com/in/kylejohnston">LinkedIn</a><br>
+									<a class="twitter" href="https://twitter.com/kylejohnston">Twitter</a><br>
+									<a class="dribbble" href="https://dribbble.com/kylejohnston">dribbble</a><br>
+									<a class="github" href="https://github.com/kylejohnston">GitHub</a>
+								</p>
+							</div>
+						</section>
+
+					</div>
+				</div>
+			</div>
+		</section>
+	</article>
+
+<?php include('includes/footer.php'); ?>
+
+<script type="text/javascript">
+	window.fitText(document.getElementById("fittext"), .60)
+	window.fitText(document.getElementById("fittext-2"), .65)
+</script>
+
+</body>
+</html>
