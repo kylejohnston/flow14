0d17845866181ef7966ae5699401e23908de9640
Kyle Johnston <flow14@gmail.com>
2018-08-20 11:58:58 -0500

    Revamped page content

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>About &times; Contact flow14 - Design Works.</title>
+	<title>About flow14 - Design Works.</title>
 	<meta name="description" content="A graphic design studio that specializes in helping small businesses, nonprofits, and political groups with logo design, website design and more." />
 	<meta name="geo.placename" content="Kansas City" />
 	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
@@ -17,65 +17,98 @@
 		<section class="block">
 			<div class="pattern__2-8-2">
 				<div class="inner">
-					<h1 id="fittext" class="center">Hello!</h1>
-					<p>flow14 is a design studio based in Kansas City. We specialize in helping your growing business, nonprofit, or political group connect with your audience with impactful design and communication.</p>
+					<h1 id="fittext" class="center">Who We Are</h1>
+					<p>flow14 is a design studio based in Kansas City. We specialize in making results-driven design that works wonders. We help your growing business, nonprofit, or political group stand out with outstanding work that moves your audience to act.</p>
 				</div>
 			</div>
 		</section>
 
 		<section class="block">
-			<div class="pattern__2-8-2">
-				<div class="inner">
-					<div class="overlay">Kyle Johnston</div>
-					<img data-src="img/kyle-johnston-700.jpg" class="lazyload" width="100%">
-					<div class="inner">
-						<p><strong>Kyle Johnston</strong> is a designer and creative leader with over 20 years of extensive experience in practicing and leading design, creative strategy, and user experience. He’s used his background in communication design, user-centered digital design, and team leadership to successfully build, manage, and mentor multi-disciplinary teams in creating strategically focused creative work for a range brands, including Garmin, Hallmark, Helzberg Diamonds, AMC Theaters, and more.</p><p> Today, he's focused on helping growing business and nonprofits engage with their audiences through customer-centered design and communication.</p>
-						<p class="text__center spacer-6--bottom"><strong>LET&rsquo;S CONNECT:</strong> <a class="linkedin" href="http://www.linkedin.com/in/kylejohnston">LinkedIn</a> &bull; <a class="twitter" href="https://twitter.com/kylejohnston">Twitter</a> &bull; <a class="dribbble" href="https://dribbble.com/kylejohnston">dribbble</a> &bull; <a class="github" href="https://github.com/kylejohnston">GitHub</a></p>
-
-						<!-- <div class="overlay">Lisa Johnston</div>
-						<img data-src="img/lisa-johnston-02.700x350.jpg" class="lazyload" width="100%">
-						<div class="inner">
-							<p><strong>Lisa Johnston</strong> is lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus tincidunt est id fringilla. In convallis commodo sem. Quisque ultricies purus et fermentum blandit. Pellentesque pellentesque est non ligula malesuada sodales. Aliquam auctor dui leo, sit amet ultrices lectus tempus euismod. Nulla facilisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin dignissim.</p>
-						</div> -->
-						<a name="contact"></a>
-						<h1 id="fittext-2">Let&rsquo;s talk.</h1>
-						<section class="block text__center">
-							<div class="col-6">
-								<h2>Email</h2>
-								<p class="center">For new business inquiries, please contact <script type="text/javascript">
-								//<![CDATA[
-								<!--
-								var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
-								"=93){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
-								".substr(0,ol);}f(\")401,\\\"v|pckv&iy),!ot610\\\\IRP]z?#\\\"\\\\($uh520\\\\" +
-								"j330\\\\{ (7+5`530\\\\bPSX420\\\\r\\\\t\\\\@YYR320\\\\_^BI610\\\\HKJYZMJ600" +
-								"\\\\d130\\\\WADJ}kn#vuz6#'b{771\\\\tQ771\\\\cbhd1e}dngh&_?ge410\\\\520\\\\\\"+
-								"\\\\\\230\\\\F[P130\\\\230\\\\020\\\\000\\\\230\\\\000\\\\600\\\\^330\\\\00" +
-								"0\\\\010\\\\100\\\\630\\\\t\\\\600\\\\410\\\\\\\"(f};o nruter};))++y(^)i(tA" +
-								"edoCrahc.x(edoCrahCmorf.gnirtS=+o;721=%y;i=+y)401==i(fi{)++i;l<i;0=i(rof;ht" +
-								"gnel.x=l,\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"                                ;
-								while(x=eval(x));
-								//-->
-								//]]>
-								</script></p>
-							</div>
-							<div class="col-6">
-								<h2>Phone</h2>
-								<p class="center">913-738-4080</p>
-							</div>
-						</section>
-
-					</div>
+<?php /* KYLE */ ?>
+			<div class="col-6">
+				<img data-src="img/kyle-johnston-700.jpg" class="lazyload spacer-2--bottom" width="100%">
+				<div class="inner inner-5">
+					<p><strong>Kyle Johnston</strong> is a designer and creative leader with over 20 years of experience in practicing and leading design, creative strategy, and user experience. He believes keeping your audience at the center of your creative strategy ensures design works to drive business results. His perspective has led to strategically smart, award-winning creative work for well-known brands like Garmin, Hallmark, AMC Theaters, and more.<br>
+					<a class="linkedin" href="http://www.linkedin.com/in/kylejohnston">LinkedIn</a> &bull; <a class="twitter" href="https://twitter.com/kylejohnston">Twitter</a> &bull; <a class="dribbble" href="https://dribbble.com/kylejohnston">dribbble</a> &bull; <a class="github" href="https://github.com/kylejohnston">GitHub</a></p>
 				</div>
 			</div>
+<?php /* LISA */ ?>
+			<div class="col-6">
+				<img data-src="img/lisa-johnston--700.jpg" class="lazyload spacer-2--bottom" width="100%">
+				<div class="inner">
+					<p><strong>Dr. Lisa Johnston</strong> holds graduate degrees in both psychology and education and brings over 25 years of experience in psychology, statistics/research, communication, and education. She believes understanding emotional impact and assessing results is vital to delivering design and messaging that excites and resonates with the audience. Lisa’s unique perspective is a core pillar of the insights and strategy that drive results for our clients at flow14.</p>
+				</div>
+			</div>
+		</section>
+<?php /* SERVICES */ ?>
+		<section class="block">
+			<div class="pattern__3-6-3">
+				<div class="inner">
+					<h1 id="fittext-3">What We Do</h1>
+					<p>Results-driven design and creative strategy means we tackle all kinds of challenges. And while the deliverables differ for every project, there are three areas where our work works wonders:</p>
+				</div>
+ 			</div>
+		</section>
+<?php /* FOCUSING */ ?>
+		<section class="block">
+			<div class="col-2 shim"></div>
+			<div class="col-2">
+				<div class="inner center__things">
+					<img class="icon" src="img/ic--focusing.svg" alt="Focusing" width="100" height="auto">
+				</div>
+			</div>
+			<div class="col-6">
+				<div class="inner inner-5">
+					<h2 class="">Focusing</h2>
+					<p>We help you see the forest for the trees and find the needle in the haystack. We ask the hard questions to understand where you're starting from and where you hope to go. Then, we plot a course and outline the communication tools you need to succeed in your journey.</p>
+					<p><strong>Typical projects include:</strong> creative strategy, brand strategy, content strategy, user experience (UX) planning, brainstorming workshops</p>
+				</div>
+			</div>
+			<div class="col-2 shim"></div>
+		</section>
+<?php /* DISTINGUISHING */ ?>
+		<section class="block">
+			<div class="col-2 shim">
+			</div>
+			<div class="col-2">
+				<div class="inner center__things">
+					<img class="icon" src="img/ic--distinguishing.svg" alt="Distinguishing" width="100" height="auto">
+				</div>
+			</div>
+			<div class="col-6">
+				<div class="inner inner-5">
+					<h2>Distinguishing</h2>
+					<p>Whether you're starting from scratch or you want to improve something you’ve made, we bring your vision to life with results-driven design that breaks through the clutter and distinguishes you from the competition. Design that reaches the right audience at the right time and moves them to engage with you.</p>
+					<p><strong>Typical projects include:</strong> branding, logo design, website design, UX design, content development</p>
+				</div>
+			</div>
+			<div class="col-2 shim"></div>
+		</section>
+<?php /* GROWING */ ?>
+		<section class="block">
+			<div class="col-2 shim"></div>
+			<div class="col-2">
+				<div class="inner center__things">
+					<img class="icon" src="img/ic--growing.svg" alt="Growing" width="100" height="auto">
+				</div>
+			</div>
+			<div class="col-6">
+				<div class="inner inner-5">
+					<h2>Growing</h2>
+					<p>Ultimately, you want growth. In revenue, customers, audience, reach. Wherever you seek it, we focus on driving it. We assess the impact of our work and help you make sense of the data. You’ll understand what moves your audience and what’s driving your growth.</p>
+					<p><strong>Typical projects include:</strong> SEO, analytics, content optimization, A/B testing</p>
+				</div>
+			</div>
+			<div class="col-2 shim"></div>
 		</section>
 	</article>
 
 <?php include('includes/footer.php'); ?>
 
 <script type="text/javascript">
-	window.fitText(document.getElementById("fittext"), .60)
-	window.fitText(document.getElementById("fittext-2"), .65)
+	window.fitText(document.getElementById("fittext"), .8)
+	window.fitText(document.getElementById("fittext-2"), 1)
+	window.fitText(document.getElementById("fittext-3"), 1)
 </script>
 
 </body>
