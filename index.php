df0986e875abce7bb4a025c3b2d8fb630020e3df
Kyle Johnston <flow14@gmail.com>
2018-08-20 12:00:18 -0500

    Revamped header

--- index.php
+++ index.php
@@ -4,19 +4,22 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>flow14 - Design Works | A Kansas City Design Studio</title>
-	<meta name="description" content="flow14 is a design studio based in Kansas City. We deliver thoughtful designs for small businesses, nonprofits, and political groups." />
+	<title>flow14 - Design Works Wonders | A Kansas City Design Studio</title>
+	<meta name="description" content="flow14 is a design studio based in Kansas City. We make design work wonders for small businesses, nonprofits, and political groups." />
 	<meta name="geo.placename" content="Kansas City" />
 	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
 	<link href="http://www.flow14.com/" rel="canonical" />
 	<?php include('includes/head.php'); ?>
 </head>
 <body class="neutral">
-	<?php include('includes/header-nav.php'); ?>
-	<section class="block">
-		<div class="pattern__2-8-2 text__center">
-			<div class="inner intro">
-				<h1>A branding and web design studio making design work for growing businesses and nonprofits.</h1>
+	<section class="welcome">
+		<?php include('includes/header-nav.php'); ?>
+		<div class="block">
+			<div class="pattern__3-6-3">
+				<div class="inner intro">
+					<h1 id="fittext">Design works wonders</h1>
+					<p class="text__center heavyweight">A branding and web design studio making design work wonders for growing businesses and nonprofits. <a href="about">Learn more about us…</a></p>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -25,14 +28,14 @@
 	<div class="block block--flush">
 		<div class="col-12 spacer-0--bottom spacer-6--top">
 			<div class="inner text__center">
-				<h2>design samples: nonprofits&horbar;</h2>
+				<h2>Design Samples: Nonprofits&horbar;</h2>
 			</div>
 		</div>
 		<div class="col-4 text__center sample">
 			<a href="hnc-living-foundation"><img src="img/samples/hnc-living/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="HNC Living Foundation Website Redesign"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="hnc-living-foundation">HNC Living</a></h2>
+					<h3><a href="hnc-living-foundation">HNC Living</a></h3>
 					<p>A website redesign with an impact.</p>
 				</div>
 			</div>
@@ -41,7 +44,7 @@
 			<a href="rounding-up-undies"><img src="img/samples/rounding-up-undies/thumbnail.jpg" width="100%" height="auto" alt="Rounding Up Undies logo design"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="rounding-up-undies">Rounding Up Undies</a></h2>
+					<h3><a href="rounding-up-undies">Rounding Up Undies</a></h3>
 					<p>Distinctive logo design for a nonprofit.</p>
 				</div>
 			</div>
@@ -50,7 +53,7 @@
 			<a href="kc-lisc"><img src="img/samples/kc-lisc/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Kansas City LISC infographic design"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="kc-lisc">KC Lisc</a></h2>
+					<h3><a href="kc-lisc">KC Lisc</a></h3>
 					<p>Interactive infographics tell a better story.</p>
 				</div>
 			</div>
@@ -61,14 +64,14 @@
 	<div class="block block--flush">
 		<div class="col-12 spacer-0--bottom spacer-6--top">
 			<div class="inner text__center">
-				<h2>design samples: websites &amp; logos&horbar;</h2>
+				<h2>Design Samples: Websites &amp; Logos&horbar;</h2>
 			</div>
 		</div>
 		<div class="col-6 text__center sample">
 			<a href="websites"><img src="img/samples/websites/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Responsive website designs"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="websites">Responsive Websites</a></h2>
+					<h3><a href="websites">Responsive Websites</a></h3>
 					<p>Mobile-friendly website designs.</p>
 				</div>
 			</div>
@@ -77,7 +80,7 @@
 			<a href="logos"><img src="img/samples/logos/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Logo designs"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="logos">Logo Designs</a></h2>
+					<h3><a href="logos">Logo Designs</a></h3>
 					<p>A selection of logo designs.</p>
 				</div>
 			</div>
@@ -95,7 +98,7 @@
 			<a href="creative-outfitters"><img src="img/samples/creative-outfitters/thumbnail.jpg" width="100%" height="auto" alt="Creative Outfitters logo design"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="creative-outfitters">Creative Outfitters</a></h2>
+					<h3><a href="creative-outfitters">Creative Outfitters</a></h3>
 					<p>Logo design for a unique startup.</p>
 				</div>
 			</div>
@@ -104,7 +107,7 @@
 			<a href="harmony-hearing-aids"><img src="img/samples/harmony/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Harmony Hearing Aids logo design"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="harmony-hearing-aids">Harmony</a></h2>
+					<h3><a href="harmony-hearing-aids">Harmony</a></h3>
 					<p>Distinctive logo design for a new business.</p>
 				</div>
 			</div>
@@ -115,14 +118,14 @@
 	<div class="block block--flush">
 		<div class="col-12 spacer-0--bottom spacer-6--top">
 			<div class="inner text__center">
-				<h2>design samples: political groups&horbar;</h2>
+				<h2>Design Samples: Political Groups&horbar;</h2>
 			</div>
 		</div>
 		<div class="col-6 text__center sample">
 			<a href="kansas-dems"><img src="img/samples/kansas-dems/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Kansas Dems logo design"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="kansas-dems">Kansas Dems</a></h2>
+					<h3><a href="kansas-dems">Kansas Dems</a></h3>
 					<p>Rebranding a political party.</p>
 				</div>
 			</div>
@@ -131,7 +134,7 @@
 			<a href="lisa-for-kansas-senate"><img src="img/samples/lisa-for-kansas-senate/thumbnail.jpg" width="100%" height="auto" class="lazyload" alt="Lisa Johnston for Kansas Senate design system"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="lisa-for-kansas-senate">Lisa for Kansas Senate</a></h2>
+					<h3><a href="lisa-for-kansas-senate">Lisa for Kansas Senate</a></h3>
 					<p>Designing a campaign for Kansas Senate.</p>
 				</div>
 			</div>
@@ -142,14 +145,14 @@
 	<div class="block block--flush">
 		<div class="col-12 spacer-0--bottom spacer-6--top">
 			<div class="inner text__center">
-				<h2>Other works&horbar;</h2>
+				<h3>Other Works&horbar;</h3>
 			</div>
 		</div>
 		<div class="col-4 text__center sample">
 			<a href="http://www.kylewjohnston.com"><img src="img/samples/video.jpg" width="100%" height="auto" class="lazyload" alt="Kyle Johnston advertising portfolio"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="http://www.kylewjohnston.com">KyleWJohnston.com</a></h2>
+					<h3><a href="http://www.kylewjohnston.com">KyleWJohnston.com</a></h3>
 					<p>My other portfolio.</p>
 				</div>
 			</div>
@@ -158,7 +161,7 @@
 			<a href="iphone-365-project"><img src="img/samples/365-2016.jpg" width="100%" height="auto" class="lazyload" alt="365 project"></a>
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
-					<h2><a href="iphone-365-project">365 Project</a></h2>
+					<h3><a href="iphone-365-project">365 Project</a></h3>
 					<p>A photo a day, every day.</p>
 				</div>
 			</div>
@@ -168,7 +171,7 @@
 			<div class="block block--flush">
 				<div class="col-12 sample-desc">
 					<h2><a href="fonts">Fonts</a></h2>
-					<p>Custom fonts &#8211; free to download.</p>
+					<p class="text__center">Custom fonts &#8211; free to download.</p>
 				</div>
 			</div>
 		</div>
@@ -178,7 +181,7 @@
 <?php include('includes/footer.php'); ?>
 
 <script type="text/javascript">
-	window.fitText(document.getElementById("fittext"), .75)
+	window.fitText(document.getElementById("fittext"), 0.8)
 </script>
 
 </body>
