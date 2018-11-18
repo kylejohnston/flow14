5a8011c374d58f379215a42ec53d31f756d9ca7e
Kyle Johnston <flow14@gmail.com>
2018-11-17 19:39:18 -0600

    Initial commit

---
+++ work.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>flow14 - Design Works.</title>
+	<meta name="description" content="Work from flow14, a graphic design studio that specializes in helping small businesses, nonprofits, and political groups with logo design, website design and more." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/work" rel="canonical" />
+    <?php include('includes/head.php'); ?>
+</head>
+<body class="neutral">
+	<?php include('includes/header-nav.php'); ?>
+	<article class="content">
+		<section class="block">
+			<div class="pattern__2-8-2">
+				<div class="inner">
+                    <h1 id="fittext" class="center">Our Design Works</h1>
+                    <p class="text__center">Things we&rsquo;ve made.</p>
+				</div>
+			</div>
+		</section>
+
+		
+<?php /* SERVICES */ ?>
+		<section class="block">
+			<div class="pattern__2-8-2 work-list">
+				<div class="inner">
+                    <h3>Nonprofits</h3>
+                    <p class="first"><a href="hnc-living-foundation">HNC Living Foundation</a> Website and logo redesign</p>
+                    <p><a href="rounding-up-undies">Rounding Up Undies</a> Logo design</p>
+                    <p><a href="kc-lisc">LISC Kansas City</a> Interactive infographic</p>
+                    <h3>Startups</h3>
+                    <p class="first"><a href="creative-outfitters">Creative Outfitters</a> Logo design, branding</p>
+                    <p><a href="harmony-hearing-aids">Harmony Hearing Aids</a> Logo design, branding</p>
+                    <h3>Design</h3>
+                    <p class="first"><a href="websites">Websites</a></p>
+                    <p><a href="logos">Logos &amp; Branding</a></p>
+                    <h3>Other Projects</h3>
+                    <p class="first"><a href="#">Noted.</a> News and musings</p>
+                    <p><a href="iphone-365-project">365 Project</a> A photo a day, every day</p>
+                    <p><a href="fonts">Fonts</a> Free to download</p>
+                    <h3>Open Source</h3>
+                    <p class="first"><a href="https://memex.flow14.com">MEMEX</a> A collection of visual inspiration</p>
+                    <p><a href="https://www.flow14.com/css-gridkit/"">GRIDkit</a> A starter kit for CSS Grid layout</p>
+                </div>
+ 			</div>
+		</section>
+	</article>
+
+<?php include('includes/footer.php'); ?>
+
+<script type="text/javascript">
+	window.fitText(document.getElementById("fittext"), .8)
+	window.fitText(document.getElementById("fittext-2"), 1)
+	window.fitText(document.getElementById("fittext-3"), 1)
+</script>
+
+</body>
+</html>
