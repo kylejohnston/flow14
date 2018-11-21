510352126553a93e01f4bd10830cca06ebf75e7c
Kyle Johnston <flow14@gmail.com>
2018-11-21 17:56:10 -0600

    Initial commit. New case study

---
+++ hnc-living-logo-redesign.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HNC Living Logo Redesign | flow14 design works.</title>
+	<meta name="description" content="Logo redesign for HNC Living Foundation, a nonprofit that partners with nonprofit healthcare providers and organizations to fund grants that help head and neck cancer patients live life fully." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/hnc-living-logo-redesign" rel="canonical" />
+	<?php include('includes/head.php'); ?>
+</head>
+<body class="neutral">
+	<?php include('includes/header-nav.php'); ?>
+	<article class="content">
+		<section class="block">
+			<div class="pattern__2-8-2">
+				<div class="inner">
+					<h1 id="fittext" class="center">HNC Living Logo Design</h1>
+				</div>
+			</div>
+		</section>
+		
+		<section class="block item">
+			<div class="col-1 shim"></div>
+			<div class="col-6">
+				<div class="inner">
+					<h2>The Ask</h2>
+					<p>After the successful redesign of the new <a href="hnc-living-foundation" title="HNC Living Website Redesign Case Study">HNC Living website</a>, the HNC Living team asked flow<small>14</small> to think about the design of their logo.</p>
+					<p>We started by meeting with the HNC Living team to talk about how the organization was evolving and their goals for the logo redesign. When we talked about what wasn&rsquo;t working with their logo, they shared a few key points:</p>
+                    <ul>
+                        <li>Their logo over-emphasized &ldquo;HNC&rdquo;</li>
+                        <li>It under-emphasized &ldquo;Living&rdquo;</li>
+                        <li>They felt the overall look was dated and could be mistaken as a logo for an accounting firm</li>
+                        <li>They didn't want to stray too far from their current design and color scheme</li>
+                    </ul>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="block block--mobile">
+					<div class="col-6">
+						<div class="inner">
+							<picture>
+								<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/logo-redesign/original-logo.png">
+								<source media="(min-width: 544px)" srcset="img/samples/hnc-living/logo-redesign/original-logo.png">
+								<img  src="img/samples/hnc-living/logo-redesign/original-logo.png"  alt="The old HNC Living Foundation logo">
+							</picture>
+							<p class="mono">The old logo</p>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="inner">
+							<picture>
+								<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/logo-redesign/new-logo.png">
+								<source media="(min-width: 544px)" srcset="img/samples/hnc-living/logo-redesign/new-logo.png">
+								<img  src="img/samples/hnc-living/logo-redesign/new-logo.png"  alt="The new HNC Living Foundation logo">
+							</picture>
+							<p class="mono">The new logo</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="block item">
+            <div class="col-3 shim"></div>
+            <div class="col-6">
+                <div class="inner">
+					<h2>Process: Exploration</h2>
+					<p>With the input from the HNC Living team mind, we started by exploring a range of designs. We tried new font combinations and emphasized &ldquo;Living&rdquo; by playing with style and scale.</p>
+				</div>
+            </div>
+            <div class="col-3 shim"></div>
+
+            <div class="col-1 shim"></div>
+			<div class="col-10">
+                <div class="inner">
+					<picture>
+						<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/logo-redesign/exploration--r01_wide.png">
+						<source media="(min-width: 544px)" srcset="img/samples/hnc-living/logo-redesign/exploration--r01_4c.png">
+						<img  src="img/samples/hnc-living/logo-redesign/exploration--r01_4c.png"  alt="HNC Living Foundation logo design explorations">
+					</picture>
+					<p class="mono">A sampling of early design explorations</p>
+                </div>
+            </div>
+            <div class="col-1 shim"></div>
+		</section>
+
+		<section class="block item">
+            <div class="col-3 shim"></div>
+            <div class="col-6">
+                <div class="inner">
+					<h2>Sharing Ideas</h2>
+					<p>After preparing a range of designs, we presented the first round of ideas to the HNC Living team. The presentation outlined key elements of each design and placed them in a website mockup to show context. Most of the designs stayed true to their existing logo, but we included an option &#8211; &rdquo;big break&ldquo; &#8211; that served a complete break from the old design. This option proved to be the favorite.</p>
+				</div>
+            </div>
+            <div class="col-3 shim"></div>
+
+			<div class="col-12">
+                <div class="inner">
+					<picture>
+						<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/logo-redesign/presentation--01.png">
+						<source media="(min-width: 544px)" srcset="img/samples/hnc-living/logo-redesign/presentation--01_2c.png">
+						<img  src="img/samples/hnc-living/logo-redesign/presentation--01_1c.png"  alt="Logo redesign presentation">
+					</picture>
+                </div>
+            </div>
+		</section>
+
+		<section class="block block--narrowitem">
+			<div class="col-1 shim"></div>
+			<div class="col-5">
+			<div class="inner">
+					<h3>Refining the design</h3>
+					<p>After chosing the new design, we explored additional variations that reintroduced &ldquo;Foundation&rdquo;</p>
+				</div>
+			</div>
+			<div class="col-5">
+				<div class="inner">
+					<picture>
+						<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/logo-redesign/presentation--02_2c.png">
+						<source media="(min-width: 544px)" srcset="img/samples/hnc-living/logo-redesign/presentation--02_2c.png">
+						<img  src="img/samples/hnc-living/logo-redesign/presentation--02_2c.png"  alt="Logo redesign presentation, round 2">
+					</picture>
+				</div>
+			</div>
+			<div class="col-1 shim"></div>
+		</section>
+
+		<section class="block item">
+			<div class="pattern__3-6-3">
+				<div class="inner">
+					<h2>Launching the Logo</h2>
+					<p>The logo was introduced in November 2018. We launched with updates to <a href="https://www.hncliving.org/" title="HNC Living website">HNCLiving.org</a> and their social profiles, a new ad layout, logo guidelines, and more.</p>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="inner">
+					<p class="mono"><a href="https://www.hncliving.org/" title="HNCLiving.org">HNCLiving.org</a></p>
+					<picture>
+						<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/logo-redesign/homepage__700.jpg">
+						<source media="(min-width: 544px)" srcset="img/samples/hnc-living/logo-redesign/website_sm.jpg">
+						<img  src="img/samples/hnc-living/logo-redesign/website_xsm.jpg"  alt="HNCLiving.org">
+					</picture>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="inner">
+					<p class="mono">Ad Layout</p>
+					<picture>
+						<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/logo-redesign/hnc-living-fall-2018-ad.jpg">
+						<source media="(min-width: 544px)" srcset="img/samples/hnc-living/logo-redesign/hnc-living-fall-2018-ad_sm.jpg">
+						<img  src="img/samples/hnc-living/logo-redesign/hnc-living-fall-2018-ad_xsm.jpg"  alt="HNC Living ad layout">
+					</picture>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="inner">
+					<p class="mono">Logo Guidelines</p>
+					<picture>
+						<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/logo-redesign/hnc-living--logo-guidelines.jpg">
+						<source media="(min-width: 544px)" srcset="img/samples/hnc-living/logo-redesign/hnc-living--logo-guidelines_sm.jpg">
+						<img  src="img/samples/hnc-living/logo-redesign/hnc-living--logo-guidelines_xsm.jpg"  alt="HNC Living logo guidelines">
+					</picture>
+				</div>
+			</div>
+		</section>
+
+		<section class="block text__center">
+			<div class="col-12">
+				<div class="inner"><?php include('includes/cta_np.php'); ?></div>
+			</div>
+		</section>
+	</article>
+
+<?php include('includes/footer.php'); ?>
+
+<script type="text/javascript">
+	window.fitText(document.getElementById("fittext"), .7)
+	window.fitText(document.getElementById("fittexttwo"), .7)
+	window.fitText(document.getElementById("fittexttre"), .7)
+</script>
+
+
+
+</body>
+</html>
