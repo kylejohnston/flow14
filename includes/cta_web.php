097b82b14572bea1ea2826d6c2535c51d2ac84a4
Kyle Johnston <flow14@gmail.com>
2018-11-21 18:03:34 -0600

    Updated copy

--- includes/cta_web.php
+++ includes/cta_web.php
@@ -1,4 +1,4 @@
 				<p class="mono">&because;</p>
 				<h2>Do you need a fresh, new website design?</h2>
-				<p>Contact me for an estimate.</p>
+				<p>Contact us for an estimate.</p>
 				<p><a class="ghost-button" href="contact">Let&rsquo;s talk.</a></p>
