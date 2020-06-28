520ce5784777dd91d8f29c2c9670e52951f327f6
Kyle Johnston <flow14@gmail.com>
2020-06-27 20:14:46 -0500

    Updated design, content, and directory

---
+++ fonts/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>+ fonts + for free + for show | flow14 design works.</title>
+	<meta name="description" content="A selection of free and open source fonts." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/fonts/" rel="canonical" />
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+</head>
+<body>
+	<div class="grid">
+		<header class="mt-2 ml-2">
+      <div>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/logo.php'); ?>
+      </div>
+		</header>
+
+		<div class="grid-intro px-2 mt-2">
+			<h1><div class="color-light-blue">Free &amp; Open Source</div>Fonts</h1>
+    </div>
+
+    <section class="main grid-in mb-4">
+      <div class="half-right grid-content">
+        <div class="img hidden shift-2 mb-2"><img class="lazyload" src="/img/fonts/sporty-1400.png" width="100%" height="auto" alt="TK"></div>
+        <h3>Sporty</h3>
+        <p>A collegiate-style display font. Includes uppercase characters with minimal punctuation.<br><a href="http://www.flow14.com/font-files/sporty.zip" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Sporty-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/spopctt.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Sporty-PC']);">Download - PC</a></p>
+      </div>
+    </section>
+    <section class="main grid-in mb-4">
+      <div class="half-left grid-content">
+        <div class="img hidden shift-2 mb-2"><img class="lazyload" src="/img/fonts/bodolive-1400.png" width="100%" height="auto" alt="TK"></div>
+        <h3>Bodolive</h3>
+        <p>A mutation of two heavyweight display fonts. Includes upper and lowercase characters and basic punctuation.<br><a href="http://www.flow14.com/font-files/bodolive.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Bodolive-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/bodpctt.zip" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Bodolive-PC']);">Download - PC</a></p>
+      </div>
+    </section>
+    <section class="main grid-in mb-4">
+      <div class="half-right grid-content">
+        <div class="img hidden shift-2 mb-2"><img class="lazyload" src="/img/fonts/meteors-1400.png" width="100%" height="auto" alt="TK"></div>
+        <h3>Meteors</h3>
+        <p>A font based on type from the classic video game Asteroids. Includes upper and lowercase characters and basic punctuation.<br><a href="http://www.flow14.com/font-files/meteors.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Meteors-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/metpctt.zip" title="download this font" onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Meteors-PC']);">Download - PC</a></p>
+      </div>
+    </section>
+    <section class="main grid-in mb-4">
+      <div class="half-left grid-content">
+        <div class="img hidden shift-2 mb-2"><img class="lazyload" src="/img/fonts/jellyphant-1400.png" width="100%" height="auto" alt="TK"></div>
+        <h3>jellyphant</h3>
+        <p>An all lowercase display font &#8212; no uppercase, no numbers, no punctuation, just basic letters.<br><a href="http://www.flow14.com/font-files/jellyphant.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Jellyphant-Mac']);">Download Mac</a> | <a href="http://www.flow14.com/font-files/jelpctt.zip" title="download this font"  onClick="_gaq.push(['_trackEvent', 'Fonts', 'Download', 'Jellyphant-PC']);">Download - PC</a></p>
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+
+  </div>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+</html>
