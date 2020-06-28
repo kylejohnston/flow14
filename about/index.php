c049723d84a7bad9f3ac0d019195f41bd3da37ed
Kyle Johnston <flow14@gmail.com>
2020-06-27 20:13:47 -0500

    Updated design, content, and directory

---
+++ about/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>About flow14 - Design Works.</title>
+	<meta name="description" content="A graphic design studio that specializes in helping small businesses, nonprofits, and political groups with strategically smart branding, website design, and more." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/about/" rel="canonical" />
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+</head>
+<body>
+	<div class="grid">
+		<header class="mt-2 ml-2">
+      <div>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/logo.php'); ?>
+      </div>
+		</header>
+
+		<div class="grid-intro px-2 mt-2">
+      <h1>flow14 is a strategically smart design studio</h1>
+      <!-- <div class="mb-4"> -->
+        <p>flow<small>14</small> is a design studio based in Kansas City with clients from across the United States. We specialize in making strategically smart, results-driven design that works wonders.</p>
+        <p class="mb-4">We can help your growing business, nonprofit, or political group stand out with outstanding work that moves your audience to act.</p>
+      <!-- </div> -->
+    </div>
+
+    <section class="main grid-in shift-2 grid-gap">
+      <div class="half-left grid-content mb-4">
+        <div class="img hidden mb-2">
+          <img class="img lazyload" src="/img/kyle-johnston-700.jpg" width="100%" height="auto" alt="Kyle Johnston">
+        </div>
+        <h3>Kyle Johnston</h3>
+        <p>Kyle  is a designer and creative leader with over 20 years of experience in practicing and leading design, creative strategy, and user experience.</p>
+        <p>He believes keeping your audience at the center of your creative strategy ensures design works to drive business results. His perspective has led to strategically smart, award-winning creative work for well-known brands like Garmin, Hallmark, AMC Theaters, and more.</p>
+        <p><a class="linkedin" href="http://www.linkedin.com/in/kylejohnston" title="Kyle Johnston on LinkedIn">LinkedIn</a> &bull; <a class="twitter" href="https://twitter.com/kylejohnston" title="@kylejohnston on Twitter">Twitter</a> &bull; <a class="dribbble" href="https://dribbble.com/kylejohnston" title="@kylejohnston on Dribbble">Dribbble</a> &bull; <a class="github" href="https://github.com/kylejohnston" title="@kylejohnston on GitHub">GitHub</a>
+        </p>
+      </div>
+      <div class="half-right grid-content mb-4">
+        <div class="img hidden mb-2">
+          <img class="img lazyload" src="/img/lisa-johnston-700.jpg" width="100%" height="auto" alt="Lisa Johnston">
+        </div>
+        <h3>Dr. Lisa Johnston</h3>
+        <p>Lisa holds graduate degrees in both psychology and education and brings over 25 years of experience in psychology, statistics/research, communication, and education.</p>
+        <p>She believes understanding emotional impact and assessing results is vital to delivering design and messaging that excites and resonates with the audience. Lisa’s unique perspective is a core pillar of the insights and strategy that drive results for our clients at flow<small>14</small>.</p>
+      </div>
+    </section>
+    <section class="main grid-in mb-4">
+      <div class="half-left mt-2">
+        <div class="grid-content sticky">
+          <h2>What We Do For You</h2>
+          <p class="mb-4">Our approach focuses on results-driven design and creative strategy means we tackle all kinds of challenges. And while the deliverables may differ for every project, there are three areas where our work works wonders:</p>
+        </div>
+      </div>
+      <div class="half-right grid-content">
+        <img class="img mb-2 hidden" src="/img/ic--focusing.svg" alt="Our Approach: Focusing" width="100%" height="auto">
+        <h3>Focusing</h3>
+        <p>We help you see the forest for the trees and find the needle in the haystack. We ask the hard questions to understand where you're starting from and where you hope to go. Then, we plot a course and outline the communication tools you need to succeed in your journey.</p>
+        <p class="mb-4"><strong>Typical projects include:</strong> creative strategy, brand strategy, content strategy, user experience (UX) planning, brainstorming workshops</p>
+        <img class="img mb-2 hidden" src="/img/ic--distinguish.svg" alt="Our Approach: Distinguishing" width="100%" height="auto">
+        <h3>Distinguishing</h3>
+        <p>Whether you're starting from scratch or you want to improve something you’ve made, we bring your vision to life with results-driven design that breaks through the clutter and distinguishes you from the competition. Design that reaches the right audience at the right time and moves them to engage with you.</p>
+        <p class="mb-4"><strong>Typical projects include:</strong> branding, logo design, website design, UX design, content development</p>
+        <img class="img mb-2 hidden" src="/img/ic--growing.svg" alt="Our Approach: Growing" width="100%" height="auto">
+        <h3>Growing</h3>
+        <p>Ultimately, you want growth. In revenue, customers, audience, reach. Wherever you seek it, we focus on driving it. We assess the impact of our work and help you make sense of the data. You’ll understand what moves your audience and what’s driving your growth.</p>
+        <p><strong>Typical projects include:</strong> SEO, analytics, content optimization, A/B testing</p>
+      </div>
+    </section>
+  </div>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+</html>
---
+++ contact/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contact flow14 - Design Works.</title>
+	<meta name="description" content="A graphic design studio that specializes in helping small businesses, nonprofits, and political groups with strategically smart branding, website design, and more." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/contact/" rel="canonical" />
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+</head>
+<body>
+	<div class="grid">
+		<header class="mt-2 ml-2">
+      <div>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/logo.php'); ?>
+      </div>
+		</header>
+
+		<div class="grid-intro px-2 mt-2" style="min-height:41ch">
+    <h1><div class="color-light-blue">Say Hello</div>Contact Us</h1>
+    </div>
+
+    <section class="main grid-in my-4 grid-gap">
+      <div class="half-left mb-4">
+        <div class="grid-content">
+          <h2>Email</h2>
+          <p class="mb-2">For new business inquiries, please contact <script type="text/javascript">
+								//<![CDATA[
+								<!--
+								var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+								"=93){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
+								".substr(0,ol);}f(\")401,\\\"v|pckv&iy),!ot610\\\\IRP]z?#\\\"\\\\($uh520\\\\" +
+								"j330\\\\{ (7+5`530\\\\bPSX420\\\\r\\\\t\\\\@YYR320\\\\_^BI610\\\\HKJYZMJ600" +
+								"\\\\d130\\\\WADJ}kn#vuz6#'b{771\\\\tQ771\\\\cbhd1e}dngh&_?ge410\\\\520\\\\\\"+
+								"\\\\\\230\\\\F[P130\\\\230\\\\020\\\\000\\\\230\\\\000\\\\600\\\\^330\\\\00" +
+								"0\\\\010\\\\100\\\\630\\\\t\\\\600\\\\410\\\\\\\"(f};o nruter};))++y(^)i(tA" +
+								"edoCrahc.x(edoCrahCmorf.gnirtS=+o;721=%y;i=+y)401==i(fi{)++i;l<i;0=i(rof;ht" +
+								"gnel.x=l,\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"                                ;
+								while(x=eval(x));
+								//-->
+								//]]>
+                </script> or use the form below.</p>
+          <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+          <script>
+          hbspt.forms.create({
+            portalId: "3292081",
+            formId: "f2f77b8f-2a83-4863-9178-c477db328fa6"
+          });
+          </script>
+        </div>
+      </div>
+      <div class="half-right mb-4">
+        <div class="grid-content">
+          <div class="mb-2">
+            <h3>Phone</h3>
+            <h3>913-738-4080</h3>
+          </div>
+          <div class="mb-2">
+            <h3>Address</h3>
+            <p>12725 W 126th St<br>Overland Park, KS 66213</p>
+          </div>
+          <div class="mb-2">
+            <h3>Elsewhere</h3>
+            <p>
+              <a class="linkedin" href="https://www.linkedin.com/company/flow14/" title="flow14 on LinkedIn">LinkedIn</a><br>
+              <a class="twitter" href="https://twitter.com/kylejohnston" title="@kylejohnston on Twitter">Twitter</a><br>
+              <a class="dribbble" href="https://dribbble.com/kylejohnston" title="@kylejohnston on Dribbble">Dribbble</a><br>
+              <a class="github" href="https://github.com/kylejohnston" title="@kylejohnston on GitHub">GitHub</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+</html>
