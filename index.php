b396de2aaf5ce55252df70b9ffe62d49bbd76630
Kyle Johnston <flow14@gmail.com>
2020-06-27 20:15:38 -0500

    New design and content

--- index.php
+++ index.php
@@ -9,75 +9,154 @@
 	<meta name="geo.placename" content="Kansas City" />
 	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
 	<link href="http://www.flow14.com/" rel="canonical" />
-	<?php include('includes/head.php'); ?>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+
 </head>
-<body class="neutral welcome-home homepage">
-	<?php include('includes/header-nav.php'); ?>
-	<section class="welcome">
-		<div class="block">
-			<div class="pattern__2-8-2">
-				<div class="inner intro">
-					<h1 id="fittext">We Make Design Work Wonders</h1>
-					<p class="text__center heavyweight">A branding and web design studio making design work wonders for growing businesses and nonprofits. <a href="about" title="Learn more about flow14">Learn more about us…</a></p>
-				</div>
-				<div class="recent"><h4>Recent Projects</h4></div>
+<body>
+	<?php// include($_SERVER['DOCUMENT_ROOT'] . '/includes/header-nav.php'); ?>
+	<div class="grid">
+		<!-- <header class="mt-2 ml-2" style="">
+		</header> -->
+		<div class="text-center home-intro mb-2" style="">
+			<div class="my-3 px-1" style="grid-column: 2 / 6;">
+				<div class="logo fill-white mb-1"><a href="https://www.flow14.com"><?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/flow14.php'); ?></a></div>
+				<h1 class="text-xxl mb-2 color-white" style="">We Make Design Work Wonders</h1>
+				<h2 class="normal-case color-white m-none" style="">We&rsquo;re a branding and web design studio making design work wonders for growing businesses and nonprofits.</h2>
+				<div class="recent"><h3>Featured Projects</h3></div>
 			</div>
 		</div>
+		<div class="gradient-block"></div>
 		<div class="color-block"></div>
-		<div class="photo-block"></div>
+	</div>
+
+	<section class="grid-home">
+			<div class="sample-lg-l hidden">
+				<div class="img img-hover">
+					<a href="/work/right-triangle/">
+						<picture class="img">
+              <source srcset="/img/samples/right-triangle/thumb-right-triangle.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/right-triangle/thumb-right-triangle.png" media="(min-width: 50em)">
+              <source srcset="/img/samples/right-triangle/thumb-right-triangle.md.webp" type="image/webp">
+              <source srcset="/img/samples/right-triangle/thumb-right-triangle.md.png">
+              <img src="/img/samples/right-triangle/thumb-right-triangle.png" width="100%" height="auto" alt="Right Triangle brand and website design" loading="lazy" />
+            </picture>
+					</a>
+				</div>
+				<div class="content">
+					<p class="text-xs uppercase">Branding, Website Design</p>
+					<h2 class="mb-1"><a href="/work/right-triangle/">Right Triangle</a></h2>
+					<p>Data designed right</p>
+				</div>
+			</div>
+
+			<div class="sample-lg-r hidden">
+				<div class="content order-2">
+					<!-- <p class="text-xs uppercase">Website Designs</p> -->
+					<h2 class="mb-1"><a href="/work/websites/">Website Designs</a></h2>
+					<p>A collection of our favorite website designs</p>
+				</div>
+				<div class="img img-hover order-1">
+					<a href="/work/websites/">
+						<picture class="img">
+              <source srcset="/img/samples/websites/thumb-websites.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/websites/thumb-websites.png" media="(min-width: 50em)">
+              <source srcset="/img/samples/websites/thumb-websites.md.webp" type="image/webp">
+              <source srcset="/img/samples/websites/thumb-websites.md.png">
+              <img class="" src="/img/samples/websites/thumb-websites.png" width="100%" height="auto" alt="Website designs" loading="lazy" />
+            </picture>
+					</a>
+				</div>
+			</div>
+
+			<div class="sample-sm-l hidden">
+				<div class="img img-hover">
+					<a href="/work/hnc-living/">
+						<picture class="img">
+              <source srcset="/img/samples/hnc-living/thumb-hnc.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/hnc-living/thumb-hnc.png" media="(min-width: 50em)">
+              <source srcset="/img/samples/hnc-living/thumb-hnc.md.webp" type="image/webp">
+              <source srcset="/img/samples/hnc-living/thumb-hnc.md.png">
+              <img src="/img/samples/hnc-living/thumb-hnc.png" width="100%" height="auto" alt="HNC Living website redesign" loading="lazy" />
+            </picture>
+					</a>
+				</div>
+
+				<div class="content">
+					<p class="text-xs uppercase">Website Redesign</p>
+					<h2 class="mb-1"><a href="/work/hnc-living/">HNC Living</a></h2>
+					<p>A more effective nonprofit website</p>
+				</div>
+			</div>
+
+			<div class="sample-sm-r hidden">
+				<div class="content order-2">
+					<h2 class="mb-1"><a href="/work/logos/">Logo Design and Branding</a></h2>
+					<p>A collection of our favorite logo designs</p>
+				</div>
+				<div class="img img-hover order-1">
+					<a href="/work/logos/">
+						<picture class="img">
+              <source srcset="/img/samples/logos/thumb-logos.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/logos/thumb-logos.png" media="(min-width: 50em)">
+              <source srcset="/img/samples/logos/thumb-logos.md.webp" type="image/webp">
+              <source srcset="/img/samples/logos/thumb-logos.md.png">
+              <img src="/img/samples/logos/thumb-logos.png" width="100%" height="auto" alt="Logo designs" loading="lazy" />
+            </picture>
+					</a>
+				</div>
+			</div>
+
+			<div class="sample-sm-l hidden">
+				<div class="img img-hover">
+					<a href="/work/rounding-up-undies/">
+						<picture class="img">
+              <source srcset="/img/samples/rounding-up-undies/thumb.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/rounding-up-undies/thumb.jpg" media="(min-width: 50em)">
+              <source srcset="/img/samples/rounding-up-undies/thumb.md.webp" type="image/webp">
+              <source srcset="/img/samples/rounding-up-undies/thumb.md.jpg">
+              <img src="/img/samples/rounding-up-undies/thumb.jpg" width="100%" height="auto" alt="Logo designs" loading="lazy" />
+            </picture>
+					</a>
+				</div>
+				<div class="content">
+					<!-- <p class="text-xs uppercase">Category</p> -->
+					<h2 class="mb-1"><a href="/work/rounding-up-undies/">Rounding Up Undies</a></h2>
+					<p>Empowering girls around the world</p>
+				</div>
+			</div>
+
+			<a class="more" href="/work/"><h3 class="m-none">More Projects <span style="letter-spacing:-3px;">――――</span></h3></a>
+
 	</section>
 
-	<section class="custom-grid spacer-6--bottom">
-		<div class="thumbnail"
-		style="grid-column: 7 / 11; grid-row: 1 / 3; height:auto; z-index:3">
-			<picture>
-				<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/hnc-living-logo-desktop.svg">
-				<source media="(min-width: 544px)" srcset="img/samples/hnc-living/hnc-living-logo__480.png">
-				<img src="img/samples/hnc-living/hnc-living-logo__480.png"  alt="HNC Living Logo">
-			</picture>
-			<div class="caption"><h3>Logo Redesign <a href="hnc-living-logo-redesign" title="Case Study: HNC Living Logo Redesign">View the Case Study</a></h3></div>
-		</div>
-		<div class="thumbnail"
-		style="grid-column: 8 / 12; grid-row: 2 / span 5; height:auto; z-index:4">
-			<picture>
-				<source media="(min-width: 1012px)" srcset="img/samples/hnc-living/homepage__700.jpg">
-				<source media="(min-width: 544px)" srcset="img/samples/hnc-living/thumbnail__480.jpg">
-				<img  src="img/samples/hnc-living/thumbnail__960.jpg"  alt="HNC Living Foundation">
-			</picture>
-			<div class="caption"><h3>Website Redesign <a href="hnc-living-foundation" title="Case Study: HNC Living Website Redesign">View the Case Study</a></h3></div>
-		</div>
-		<div class="thumbnail"
-		style="display:grid; grid-column: 3 / 7; grid-row: 2 / 3; height:auto; align-content:end;">
-			<img src="img/samples/rounding-up-undies/thumbnail.jpg" width="100%" height="auto" alt="Rounding Up Undies logo design">
-			<div class="caption"><h3>Logo Design <a href="rounding-up-undies" title="Case Study: Rounding Up Undies Logo Design">View the Case Study</a></h3></div>
-		</div>
-		<div class="thumbnail"
-		style="grid-column: 2 / 8; grid-row: 3 / 5; height:auto;">
-			<picture>
-				<source media="(min-width: 1012px)" srcset="img/samples/kc-lisc/thumbnail__01.jpg">
-				<source media="(min-width: 544px)" srcset="img/samples/kc-lisc/thumbnail__01.jpg">
-				<img  src="img/samples/kc-lisc/thumbnail__01.jpg"  alt="LISC Kansas City Interactive Infographic">
-			</picture>
-			<div class="caption"><h3>Interactive Infographic <a href="kc-lisc" title="Case Study: LISC Kansas City Interactive Infographic">View the Case Study</a></h3></div>
-		</div>
-		<div class="thumbnail"
-		style="grid-column: 4 / 8; grid-row: 5 / span 3; height:auto;">
-			<img src="img/samples/creative-outfitters/business-card.jpg" width="100%" height="auto" alt="Creative Outfitters branding">
-			<div class="caption"><h3>Logo Design &amp; Branding <a href="creative-outfitters" title="Case Study: Creative Outfitters branding">View the Case Study</a></h3></div>
-		</div>
-		<div class="thumbnail"
-		style="display:grid; align-content:start; grid-column: 8 / 12; grid-row: 7 / span 1; justify-content:center; align-content:center;">
-			<a href="work" title="View more design work made by flow14">
-			<div class="more"><h4>More Work</h4></div>
-			</a>
+	<section class="grid">
+		<div class="main grid-in hidden">
+			<h2 class="text-center brand-title mb-3" style="">Brands We&rsquo;ve Worked With:</h2>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/garmin-logo.svg" alt="Garmin"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/hallmark-logo.svg" alt="Hallmark"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/amc-logo.svg" alt="AMC"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/boston-scientific-logo.svg" alt="Boston Scientific"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/vista-outdoor-logo.svg" alt="Vista Outdoor"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/bushnell-logo.svg" alt="Bushnell"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/hv-logo-h.svg" alt="Happy Valley"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/deg-logo.svg" alt="DEG"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/grayduck-logo.svg" alt="Grayduck Health"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/inquest-logo.svg" alt="InQuest Marketing"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/sidebench-logo.svg" alt="Sidebench"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/qwally-logo.svg" alt="Qwally"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/hnc-living-logo.svg" alt="HNC Living"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/one-global-village.svg" alt="One Global Village"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/right-triangle-logo.svg" alt="Right Triangle"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/rounding-up-undies.svg" alt="Rounding Up Undies"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/vmc-group-logo.svg" alt="VMC Group"></div>
+			<div class="brand" style=""><img loading="lazy" width="70%" height="70%" src="/img/brands/stay-alfred-logo.svg" alt="Stay Alfred"></div>
 		</div>
 	</section>
 
-<?php include('includes/footer.php'); ?>
 
-<script type="text/javascript">
-	window.fitText(document.getElementById("fittext"), 0.8)
-</script>
+
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
 
 </body>
 </html>
