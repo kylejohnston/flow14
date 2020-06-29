02d88ec87e1c36dcd9c0de6901792046bebd13fd
Kyle Johnston <flow14@gmail.com>
2020-06-29 02:28:13 -0500

    New layout

---
+++ 404/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Oops. | flow14. A Design Studio.</title>
+	<meta name="description" content="A graphic design studio that specializes in helping small businesses, nonprofits, and political groups with strategically smart branding, website design, and more." />
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+</head>
+<body>
+	<div class="grid">
+		<header class="mt-2 ml-2">
+      <div>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/logo.php'); ?>
+      </div>
+		</header>
+
+		<div class="grid-intro px-2 mt-2" style="min-height:41ch">
+    <h1><div class="color-light-blue">Oops…</div>Page Not Found.</h1>
+    </div>
+
+    <section class="main grid-in mt-3 mb-4 grid-gap">
+      <div class="mb-4" style="grid-column: 2 / 5">
+        <div class="px-2">
+          <p>Sorry, the page you’re looking for is has moved or is no longer available. What&rsquo;s next? Go <a href="/">back to the homepage</a>, check out <a href="/work/">recent work</a>, or <a href="/contact/">get in touch</a> with any questions.</p>
+
+        </div>
+      </div>
+
+    </section>
+  </div>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+</html>
