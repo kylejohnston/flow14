0524bcef3a8d595f6b0aa43fae93a48ab222d3ec
Kyle Johnston <flow14@gmail.com>
2020-07-16 16:40:12 -0500

    Added new case study, testimonials

--- work/index.php
+++ work/index.php
@@ -32,14 +32,41 @@
               <source srcset="/img/samples/right-triangle/thumb-right-triangle.png" media="(min-width: 50em)">
               <source srcset="/img/samples/right-triangle/thumb-right-triangle.md.webp" type="image/webp">
               <source srcset="/img/samples/right-triangle/thumb-right-triangle.md.png">
-              <img src="/img/samples/right-triangle/thumb-right-triangle.png" width="100%" height="auto" alt="Right Triangle brand and website design" loading="lazy" />
+              <img src="/img/samples/right-triangle/thumb-right-triangle.png" width="100%" height="auto" alt="Right Triangle brand and website design." loading="lazy" />
             </picture>
           </a>
         </div>
         <h3><a href="/work/right-triangle/">Branding and Website Design</a></h3>
       </div>
-
       <div class="half-right mb-4">
+        <div class="img img-hover hidden mb-1">
+          <a href="/work/qwally/">
+            <picture class="img">
+              <source srcset="/img/samples/qwally/thumb.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/qwally/thumb.png" media="(min-width: 50em)">
+              <source srcset="/img/samples/qwally/thumb.md.webp" type="image/webp">
+              <source srcset="/img/samples/qwally/thumb.md.png">
+            <img src="/img/samples/qwally/thumb.png" width="100%" height="auto" alt="UX and website design for Qwally." loading="lazy" />
+            </picture>
+          </a>
+        </div>
+        <h3><a href="/work/qwally/">UX and Website Design</a></h3>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-4">
+      <div class="testimonial">
+				<div class="source pl-1 pr-1 order-2">
+					<p><strong>Chris Offensend</strong><br>Co-Founder &amp; CEO<br>Qwally</p>
+				</div>
+				<div class="quote pl-1 pr-1 order-1">
+					<p>"We were looking to turn some loose ideas and research into a software platform for city governments. What made flow14 the easy choice for us was Kyle's focus on fundamentally understanding the problem to be solved and shepherding our team (and our clients) through that process to arrive at a solution that truly helped users accomplish their goals. As we&rsquo;ve grown and encountered new challenges and opportunities, we&rsquo;ve been grateful to consider flow14 as a trusted advisor and direct extension of our team."</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="main grid-in grid-gap text-center">
+      <div class="half-left mb-4">
         <div class="img img-hover hidden mb-1">
           <a href="/work/websites/">
             <picture class="img">
@@ -47,11 +74,67 @@
               <source srcset="/img/samples/websites/thumb-websites.png" media="(min-width: 50em)">
               <source srcset="/img/samples/websites/thumb-websites.md.webp" type="image/webp">
               <source srcset="/img/samples/websites/thumb-websites.md.png">
-              <img src="/img/samples/websites/thumb-websites.png" width="100%" height="auto" alt="Website Design Samples" loading="lazy" />
+              <img src="/img/samples/websites/thumb-websites.png" width="100%" height="auto" alt="Website Design Samples." loading="lazy" />
             </picture>
           </a>
         </div>
         <h3><a href="/work/websites/">Website Designs</a></h3>
+      </div>
+      <div class="half-right mb-4">
+        <div class="img img-hover hidden mb-1">
+          <a href="/work/logos/">
+            <picture class="img">
+              <source srcset="/img/samples/logos/thumb-logos.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/logos/thumb-logos.png" media="(min-width: 50em)">
+              <source srcset="/img/samples/logos/thumb-logos.md.webp" type="image/webp">
+              <source srcset="/img/samples/logos/thumb-logos.md.png">
+              <img src="/img/samples/logos/thumb-logos.png" width="100%" height="auto" alt="Logo designs." loading="lazy" />
+            </picture>
+          </a>
+        </div>
+        <h3><a href="/work/logos/">Logo Designs</a></h3>
+      </div>
+    </section>
+
+    <section class="main grid-in grid-gap text-center">
+      <div class="half-left mb-4">
+        <div class="img img-hover hidden mb-1">
+          <a href="/work/hnc-living/">
+            <picture class="img">
+              <source srcset="/img/samples/hnc-living/thumb-hnc.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/hnc-living/thumb-hnc.png" media="(min-width: 50em)">
+              <source srcset="/img/samples/hnc-living/thumb-hnc.md.webp" type="image/webp">
+              <source srcset="/img/samples/hnc-living/thumb-hnc.md.png">
+              <img src="/img/samples/hnc-living/thumb-hnc.png" width="100%" height="auto" alt="HNC Living website design." loading="lazy" />
+            </picture>
+          </a>
+        </div>
+        <h3><a href="/work/hnc-living/">Website Redesign</a></h3>
+      </div>
+      <div class="half-right mb-4">
+        <div class="img img-hover hidden mb-1">
+          <a href="/work/rounding-up-undies/">
+            <picture class="img">
+              <source srcset="/img/samples/rounding-up-undies/thumb.webp" media="(min-width: 50em)" type="image/webp">
+              <source srcset="/img/samples/rounding-up-undies/thumb.jpg" media="(min-width: 50em)">
+              <source srcset="/img/samples/rounding-up-undies/thumb.md.webp" type="image/webp">
+              <source srcset="/img/samples/rounding-up-undies/thumb.md.jpg">
+              <img src="/img/samples/rounding-up-undies/thumb.jpg" width="100%" height="auto" alt="Rounding Up Undies logo design." loading="lazy" />
+            </picture>
+          </a>
+        </div>
+        <h3><a href="/work/rounding-up-undies/">Logo Design</a></h3>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-4">
+      <div class="testimonial">
+				<div class="source pl-1 pr-1 order-2">
+					<p><strong>Tom Walsh</strong><br>Co-Founder<br>HNC Living Foundation</p>
+				</div>
+				<div class="quote pl-1 pr-1 order-1">
+					<p>“flow14 did an excellent job of laying out the opportunities for improving our website. Kyle clearly identified objectives and planned an approach we could follow and participate in that didn’t disrupt our normal activities. Our finished product came in on-time, on-budget, and on-target. We will most definitely continue to rely on Kyle and flow14 to keep our web presence current and designed to enhance our brand and identity.”</p>
+        </div>
       </div>
     </section>
 
@@ -64,7 +147,7 @@
               <source srcset="/img/samples/harmony/thumb.jpg" media="(min-width: 50em)">
               <source srcset="/img/samples/harmony/thumb.md.webp" type="image/webp">
               <source srcset="/img/samples/harmony/thumb.md.jpg">
-              <img src="/img/samples/harmony/thumb.jpg" width="100%" height="auto" alt="Harmony Hearing Aids logo design" loading="lazy" />
+              <img src="/img/samples/harmony/thumb.jpg" width="100%" height="auto" alt="Harmony Hearing Aids logo design." loading="lazy" />
             </picture>
           </a>
         </div>
@@ -79,7 +162,7 @@
               <source srcset="/img/samples/creative-outfitters/thumb.jpg" media="(min-width: 50em)">
               <source srcset="/img/samples/creative-outfitters/thumb.md.webp" type="image/webp">
               <source srcset="/img/samples/creative-outfitters/thumb.md.jpg">
-              <img src="/img/samples/creative-outfitters/thumb.jpg" width="100%" height="auto" alt="Creative Outfitters Aids logo design" loading="lazy" />
+              <img src="/img/samples/creative-outfitters/thumb.jpg" width="100%" height="auto" alt="Creative Outfitters logo design." loading="lazy" />
             </picture>
           </a>
         </div>
@@ -94,7 +177,7 @@
               <source srcset="/img/samples/kc-lisc/thumb.jpg" media="(min-width: 50em)">
               <source srcset="/img/samples/kc-lisc/thumb.md.webp" type="image/webp">
               <source srcset="/img/samples/kc-lisc/thumb.md.jpg">
-              <img src="/img/samples/kc-lisc/thumb.jpg" width="100%" height="auto" alt="Creative Outfitters Aids logo design" loading="lazy" />
+              <img src="/img/samples/kc-lisc/thumb.jpg" width="100%" height="auto" alt="Data Visualization for Kansas City LISC." loading="lazy" />
             </picture>
           </a>
         </div>
@@ -103,50 +186,6 @@
     </section>
 
     <section class="main grid-in grid-gap text-center">
-      <div class="half-left mb-4">
-        <div class="img img-hover hidden mb-1">
-          <a href="/work/logos/">
-            <picture class="img">
-              <source srcset="/img/samples/logos/thumb-logos.webp" media="(min-width: 50em)" type="image/webp">
-              <source srcset="/img/samples/logos/thumb-logos.png" media="(min-width: 50em)">
-              <source srcset="/img/samples/logos/thumb-logos.md.webp" type="image/webp">
-              <source srcset="/img/samples/logos/thumb-logos.md.png">
-              <img src="/img/samples/logos/thumb-logos.png" width="100%" height="auto" alt="Logo designs" loading="lazy" />
-            </picture>
-          </a>
-        </div>
-        <h3><a href="/work/logos/">Logo Designs</a></h3>
-      </div>
-
-      <div class="half-right mb-4">
-        <div class="img img-hover hidden mb-1">
-          <a href="/work/hnc-living/">
-            <picture class="img">
-              <source srcset="/img/samples/hnc-living/thumb-hnc.webp" media="(min-width: 50em)" type="image/webp">
-              <source srcset="/img/samples/hnc-living/thumb-hnc.png" media="(min-width: 50em)">
-              <source srcset="/img/samples/hnc-living/thumb-hnc.md.webp" type="image/webp">
-              <source srcset="/img/samples/hnc-living/thumb-hnc.md.png">
-              <img src="/img/samples/hnc-living/thumb-hnc.png" width="100%" height="auto" alt="HNC Living website design" loading="lazy" />
-            </picture>
-          </a>
-        </div>
-        <h3><a href="/work/hnc-living/">Website Redesign</a></h3>
-      </div>
-
-      <div class="column mb-4">
-        <div class="img img-hover hidden mb-1">
-          <a href="/work/rounding-up-undies/">
-            <picture class="img">
-              <source srcset="/img/samples/rounding-up-undies/thumb.webp" media="(min-width: 50em)" type="image/webp">
-              <source srcset="/img/samples/rounding-up-undies/thumb.jpg" media="(min-width: 50em)">
-              <source srcset="/img/samples/rounding-up-undies/thumb.md.webp" type="image/webp">
-              <source srcset="/img/samples/rounding-up-undies/thumb.md.jpg">
-              <img src="/img/samples/rounding-up-undies/thumb.jpg" width="100%" height="auto" alt="Rounding Up Undies logo design" loading="lazy" />
-            </picture>
-          </a>
-        </div>
-        <h3><a href="/work/rounding-up-undies/">Logo Design</a></h3>
-      </div>
       <div class="column mb-4">
         <div class="img img-hover hidden mb-1">
           <a href="/fonts/">
