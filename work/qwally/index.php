266db728a54fcb048c03c100b17218ea6b12cc60
Kyle Johnston <flow14@gmail.com>
2020-07-16 16:40:20 -0500

    New case study

---
+++ work/qwally/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Qwally UX and Website Design | flow14. A Design Studio.</title>
+	<meta name="description" content="User experience and website design for Qwally, a cloud-based software startup that helps small businesses  navigate local government contracting." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/work/qwally/" rel="canonical" />
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+</head>
+<body>
+	<div class="grid">
+		<header class="mt-2 ml-2">
+      <div>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/logo.php'); ?>
+    </div>
+		</header>
+
+		<div class="grid-intro px-2 mt-2">
+			<h1><div class="color-light-blue">Inclusive Contracting Made Easy</div>Qwally</h1>
+      <p>Qwally is a cloud-based software startup that helps small businesses navigate local government contracting.</p>
+      <p class="mb-4">They were developing a SaaS platform for local governments, starting with the City of Mobile, Alabama. They connected with flow14 to define and design the user experience, including the user journey, information architecture, wireframes, and visual design.</p>
+    </div>
+
+    <section class="main grid-in mb-2 grid-gap shift-2">
+      <div class="grid-content">
+        <picture class="img hidden">
+          <source srcset="/img/samples/qwally/user-journey-02.lg.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/user-journey-02.jpg" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/user-journey-02.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/user-journey-02.sm.jpg">
+          <img class="" src="/img/samples/qwally/user-journey-02.jpg" width="100%" height="auto" alt="Qwally meeting with Mobile Alabama city officials." loading="lazy">
+        </picture>
+        <div class="text-sm text-center mt-1 mb-2">Qwally meeting with Mobile, AL City Officials</div>
+      </div>
+      <div class="half-left grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/user-flow-story.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/user-flow-story.jpg" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/user-flow-story.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/user-flow-story.sm.jpg">
+          <img class="shadow" src="/img/samples/qwally/user-flow-story.jpg" width="100%" height="auto" alt="User story." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-right grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/user-journey-01.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/user-journey-01.jpg" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/user-journey-01.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/user-journey-01.sm.jpg">
+          <img class="" src="/img/samples/qwally/user-journey-01.jpg" width="100%" height="auto" alt="User story and user flow diagram." loading="lazy">
+        </picture>
+      </div>
+    </section>
+    <section class="main grid-in mb-4">
+      <div class="half-right grid-content">
+        <h3>Visualizing the User Journey</h3>
+        <p>We started by creating a detailed user journey diagram to visualize the steps that could be required of a business owner seeking to work with the city.</p>
+        <p>The user journey diagram was key in Qwally&rsquo;s conversations with city officials, uncovering insights that informed user flows and information architecture, improving the experience for both businesses and the city.</p>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-4">
+      <div class="testimonial">
+				<div class="source pl-1 pr-1 order-2">
+					<p><strong>Chris Offensend</strong><br>Qwally Co-Founder &amp; CEO</p>
+				</div>
+				<div class="quote pl-1 pr-1 order-1">
+					<p>"We were looking to turn some loose ideas and research into a software platform for city governments. What made flow14 the easy choice for us was Kyle's focus on fundamentally understanding the problem to be solved and shepherding our team (and our clients) through that process to arrive at a solution that truly helped users accomplish their goals. As we&rsquo;ve grown and encountered new challenges and opportunities, we&rsquo;ve been grateful to consider flow14 as a trusted advisor and direct extension of our team."</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-2 grid-gap">
+      <div class="grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/user-flow-01.lg.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/user-flow-01.jpg" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/user-flow-01.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/user-flow-01.sm.jpg">
+          <img class="" src="/img/samples/qwally/user-flow-01.jpg" width="100%" height="auto" alt="Sample user flow." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-left grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/user-flow-02.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/user-flow-02.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/user-flow-02.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/user-flow-02.sm.png">
+          <img class="" src="/img/samples/qwally/user-flow-02.png" width="100%" height="auto" alt="Wireframe: referral dashboard." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-right grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/user-flow-03.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/user-flow-03.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/user-flow-03.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/user-flow-03.sm.png">
+          <img class="" src="/img/samples/qwally/user-flow-03.png" width="100%" height="auto" alt="Wireframes: referral emails" loading="lazy">
+        </picture>
+      </div>
+    </section>
+    <section class="main grid-in mb-4">
+      <div class="half-right grid-content">
+        <h3>User Flows</h3>
+        <p>For Qwally's presentations to city officials in Mobile, we created sample user flows and wireframes that demonstrated how Qwally’s software could improve the flow of communication between key stakeholders across the city.</p>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-2 grid-gap">
+      <div class="half-left grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-01.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-01.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-01.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-01.sm.png">
+          <img class="shadow" src="/img/samples/qwally/wireframe-dashboard-01.png" width="100%" height="auto" alt="Wireframe: dashboard concept 1." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-right grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-02.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-02.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-02.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-02.sm.png">
+          <img class="shadow" src="/img/samples/qwally/wireframe-dashboard-02.png" width="100%" height="auto" alt="Wireframe: dashboard concept 2." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-left grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-03.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-03.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-03.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-03.sm.png">
+          <img class="shadow" src="/img/samples/qwally/wireframe-dashboard-03.png" width="100%" height="auto" alt="Wireframe: dashboard concept 3." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-right grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-04.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-04.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-04.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-dashboard-04.sm.png">
+          <img class="shadow" src="/img/samples/qwally/wireframe-dashboard-04.png" width="100%" height="auto" alt="Wireframe: dashboard concept 4." loading="lazy">
+        </picture>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-4">
+      <div class="half-left grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/wireframe-page.webp" media="(min-width: 63em)" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-page.png" media="(min-width: 63em)">
+          <source srcset="/img/samples/qwally/wireframe-page.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/wireframe-page.sm.png">
+          <img class="shadow" src="/img/samples/qwally/wireframe-page.png" width="100%" height="auto" alt="Wireframe: public-facing webpage." loading="lazy" />
+        </picture>
+      </div>
+      <div class="half-right my-2">
+        <div class="grid-content sticky">
+          <h3>Wireframes: Dashboard and Frontend Concepts</h3>
+          <p>Informed by the user journeys and insights from Qwally’s meetings with city officials, we created a range of wireframes to define the user experience. These included concepts for a dashboard used by city officials, details of key user flows, and public-facing pages.</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-2">
+      <div class="half-left grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/quick-start-wire-01.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/quick-start-wire-01.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/quick-start-wire-01.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/quick-start-wire-01.sm.png">
+          <img class="" src="/img/samples/qwally/quick-start-wire-01.png" width="100%" height="auto" alt="Wireframe: Quick Start Plan, screen 1." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-right grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/quick-start-wire-02.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/quick-start-wire-02.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/quick-start-wire-02.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/quick-start-wire-02.sm.png">
+          <img class="" src="/img/samples/qwally/quick-start-wire-02.png" width="100%" height="auto" alt="Wireframe: Quick Start Plan, screen 2." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-left grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/quick-start-01.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/quick-start-01.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/quick-start-01.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/quick-start-01.sm.png">
+          <img class="" src="/img/samples/qwally/quick-start-01.png" width="100%" height="auto" alt="Visual Design: Quick Start Plan welcome screen." loading="lazy">
+        </picture>
+      </div>
+      <div class="half-right grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/quick-start-02.webp" media="(min-width: 50em)" type="image/webp">
+          <source srcset="/img/samples/qwally/quick-start-02.png" media="(min-width: 50em)">
+          <source srcset="/img/samples/qwally/quick-start-02.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/quick-start-02.sm.png">
+          <img class="" src="/img/samples/qwally/quick-start-02.png" width="100%" height="auto" alt="Visual Design: Quick Start Plan question screen." loading="lazy">
+        </picture>
+      </div>
+    </section>
+    <section class="main grid-in mb-4">
+      <div class="half-right grid-content">
+        <h3>Quick Start Plan UX</h3>
+        <p>Based on the user flow mapping, feedback from city officials, and Qwally’s user testing, we created a quiz-like experience for business owners that quickly leads them through a series of relevant questions.</p>
+        <p>After completing the quiz, users receive a personalized checklist they can use to ensure they have everything they need to be eligible to work with the city.</p>
+        <p>The final UX and design presents a series of simple questions that business owners can complete in minutes, a huge time saving over the previous process, which required lengthy in-person meetings.</p>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-4">
+      <div class="half-left grid-content">
+        <picture class="img mb-2 hidden">
+          <source srcset="/img/samples/qwally/portal.webp" media="(min-width: 63em)" type="image/webp">
+          <source srcset="/img/samples/qwally/portal.jpg" media="(min-width: 63em)">
+          <source srcset="/img/samples/qwally/portal.sm.webp" type="image/webp">
+          <source srcset="/img/samples/qwally/portal.sm.jpg">
+          <img class="shadow" src="/img/samples/qwally/portal.jpg" width="100%" height="auto" alt="Work with Mobile portal design." loading="lazy" />
+        </picture>
+      </div>
+      <div class="half-right my-2">
+        <div class="grid-content sticky">
+          <h3>Work with Mobile Portal</h3>
+          <p>When Qwally was ready to launch their solution for the City of Mobile, we designed a portal that features the Quick Start Questionnaire and a range of SEO-optimized educational resources.</p>
+          <p>Today, we continue to work with Qwally to iterate and improve on their platform as they bring their innovative tools to a growing list of cities.</p>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="main grid-in mb-4">
+      <div class="half-right grid-content">
+        <h3>Need a fresh visual identity for your brand?</h3>
+        <a class="cta my-1" href="/contact/" style=""><h3 class="m-none">Let&rsquo;s Talk<span style="letter-spacing:-3px;">――――</span></h3></a>
+      </div>
+    </section>
+
+  </div>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+</html>
