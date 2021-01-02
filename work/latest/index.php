0eb5dcbeef5a73e4ce396ea86f3cb44b1994906b
Kyle Johnston <flow14@gmail.com>
2021-01-02 15:51:50 -0600

    New page, initial commit

---
+++ work/latest/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Latest Work | flow14. A Design Studio.</title>
+	<meta name="description" content="Examples of responsive, mobile-friendly websites we've designed and developed for various small businesses and political groups." />
+	<meta name="geo.placename" content="Kansas City" />
+	<link href="https://plus.google.com/103758963959064535451" rel="publisher" />
+	<link href="http://www.flow14.com/work/hnc-living/" rel="canonical" />
+  <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'); ?>
+</head>
+<body>
+	<div class="grid">
+		<header class="mt-2 ml-2">
+      <div>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/logo.php'); ?>
+      </div>
+		</header>
+
+		<div class="grid-intro px-2 mt-2">
+			<h1>Latest Work</h1>
+      <p>A snapshot of our newest work. Full case studies coming soon.</p>
+    </div>
+
+    <section class="main grid-in mb-4">
+      <div class="half-left grid-content order-2" style="z-index:2">
+        <picture class="img hidden mb-2">
+          <source srcset="/img/samples/latest/get-konnected-home.lg.webp" media="(min-width: 63em)" type="image/webp">
+          <source srcset="/img/samples/latest/get-konnected-home.lg.jpg" media="(min-width: 63em)">
+          <source srcset="/img/samples/latest/get-konnected-home.webp" type="image/webp">
+          <source srcset="/img/samples/latest/get-konnected-home.jpg">
+          <img class="shadow" src="/img/samples/latest/get-konnected-home.lg.jpg" width="100%" height="auto" alt="Get Konnected Branding and Website Design" loading="lazy" />
+        </picture>
+      </div>
+      <div class="half-right my-2 order-1">
+        <div class="grid-content sticky">
+          <h3>Get Konnected!</h3>
+          <p>A new brand and website design for Get Konnected, a Boston-based social impact venture.</p>
+          <p>Combining the design expertise of flow14 and an innovative technology platform developed by Qwally, we launched the new Get Konnected website in November 2020.</p>
+          <p>The easy-to-use, mobile-optimized website features the GK Market, an innovative marketplace that supports small businesses in Boston and beyond.</p>
+          <p><a href="https://getkonnected.com/">Visit GetKonnected.com</a></p>
+        </div>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-4">
+      <div class="testimonial">
+				<div class="source pl-1 pr-1 order-2">
+					<p><strong>Mayor Marty Walsh</strong><br>City of Boston</p>
+				</div>
+				<div class="quote pl-1 pr-1 order-1">
+					<p>“Get Konnected’s new digital marketplace is an incredible tool that will increase the visibility of local black and brown businesses, allowing our businesses to be more accessible to the public.”</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-4">
+      <div class="half-left my-2">
+        <div class="grid-content sticky">
+          <h3>Northland OBGYN</h3>
+          <p>Website redesign for a Kansas City-based OBGYN clinic. The new site features an SEO-friendly content structure and a modern, mobile-optimized design.</p>
+        </div>
+      </div>
+      <div class="half-right grid-content">
+        <picture class="img hidden mb-2">
+          <source srcset="/img/samples/latest/northland-obgyn.lg.webp" media="(min-width: 63em)" type="image/webp">
+          <source srcset="/img/samples/latest/northland-obgyn.lg.jpg" media="(min-width: 63em)">
+          <source srcset="/img/samples/latest/northland-obgyn.webp" type="image/webp">
+          <source srcset="/img/samples/latest/northland-obgyn.jpg">
+          <img class="shadow" src="/img/samples/latest/northland-obgyn.jpg" width="100%" height="auto" alt="Northland OBGYN website design" loading="lazy" />
+        </picture>
+      </div>
+    </section>
+
+    <section class="main grid-in mb-4">
+      <picture class="full mb-2 hidden">
+        <source srcset="/img/samples/latest/happy-valley-flyers.lg.webp" media="(min-width: 63em)" type="image/webp">
+        <source srcset="/img/samples/latest/happy-valley-flyers.lg.jpg" media="(min-width: 63em)">
+        <source srcset="/img/samples/latest/happy-valley-flyers.webp" type="image/webp">
+        <source srcset="/img/samples/latest/happy-valley-flyers.jpg">
+        <img class="" src="/img/samples/latest/happy-valley-flyers.jpg" width="100%" height="auto" alt="Happy Valley Holiday Flyers" loading="lazy" />
+      </picture>
+      <div class="half-right grid-content mb-3">
+        <h3>Happy Valley Holiday Flyers</h3>
+        <p>A series of Holiday Flyers designed for Happy Valley, a Boston-based cannabis dispensary. The designs were used for both printed flyers and in-store digital displays.</p>
+      </div>
+    </section>
+
+
+    <!-- <section class="main grid-in mb-4">
+      <div class="full mb-2">
+        <img class="" src="/img/samples/latest/happy-valley-flyers.lg.jpg" alt="">
+      </div>
+      <div class="half-right grid-content mb-3">
+        <h3>Competitive Research</h3>
+        <p>We researched scores of nonprofits in cancer care and other areas to understand different models for site organization, content, and calls to action.</p>
+      </div>
+
+    </section> -->
+
+    <!-- <div class="full grid-in mb-4">
+      <img src="/img/samples/latest/happy-valley-flyers.lg.jpg" alt="">
+    </div> -->
+
+
+    <section class="main grid-in mb-4">
+      <div class="half-right grid-content">
+        <h3>Ready for design that works wonders for your company?</h3>
+        <a class="cta my-1" href="/contact/" style=""><h3 class="m-none">Let&rsquo;s Talk<span style="letter-spacing:-3px;">――――</span></h3></a>
+      </div>
+    </section>
+
+  </div>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
+</body>
+</html>
