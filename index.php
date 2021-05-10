edd65fff4d1c31149e4cbbca0467104dafb3a82f
Kyle Johnston <flow14@gmail.com>
2021-05-10 13:20:38 -0500

    Updated current WIP

--- index.php
+++ index.php
@@ -25,35 +25,35 @@
 	</header>
 
 	<div class="grid">
-		<div class="home-intro px-2 bg-gradient shift-right">
+		<div class="px-2 home-intro bg-gradient shift-right">
 			<div class="my-3 shift-right-content" style="grid-column: 2 / 6;">
-				<div class="logo fill-black mb-1"><?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/flow14.php'); ?></div>
-				<h1 class="text-xxl mb-2 color-white" style="">Design That Makes a Difference</h1>
-				<h2 class="normal-case color-white mr-3" style="margin-bottom:0">We stay busy by designing things that drive results for your business.</h2>
+				<div class="mb-1 logo fill-black"><?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/flow14.php'); ?></div>
+				<h1 class="mb-2 text-xxl color-white" style="">Design That Makes a Difference</h1>
+				<h2 class="mr-3 normal-case color-white" style="margin-bottom:0">We stay busy by designing things that drive results for your business.</h2>
 			</div>
 		</div>
 		<div class="color-block"></div>
 	</div>
 
-	<h2 class="text-center brand-title my-3">How We Do It</h2>
+	<h2 class="my-3 text-center brand-title">How We Do It</h2>
 
-	<div data-butter-container="bSlider" class="slider mb-4">
+	<div data-butter-container="bSlider" class="mb-4 slider">
 		<div data-butter-slidable="bSlider" class="slides">
-			<div class="slide text-center">
+			<div class="text-center slide">
 				<img class="mb-2 text-center" src="/img/ic-focus.svg" alt="Focus" width="50%" height="auto">
 				<div class="hidden">
 					<h2 class="text-center" style="margin-bottom:.25em;">Vision</h2>
 					<p class="text-center" style="margin-bottom:.25em;">Creative and user experience strategy</p>
 				</div>
 			</div>
-			<div class="slide text-center">
+			<div class="text-center slide">
 				<img class="mb-2 text-center" src="/img/ic-distinguish.svg" alt="Distinguish" width="50%" height="auto">
 				<div class="hidden">
 					<h2 class="text-center" style="margin-bottom:.25em;">Action</h2>
 					<p class="text-center" style="margin-bottom:.25em;">UX, visual design, and branding</p>
 				</div>
 			</div>
-			<div class="slide text-center">
+			<div class="text-center slide">
 				<img class="mb-2 text-center" src="/img/ic-grow.svg" alt="Grow" width="50%" height="auto">
 				<div class="hidden">
 					<h2 class="text-center" style="margin-bottom:.25em;">Optimization</h2>
@@ -64,30 +64,29 @@
 	</div>
 
 	<div class="grid">
-		<section class="main grid-in mb-4">
+		<section class="mb-4 main grid-in">
 			<div class="half-left grid-content" style="">
 				<h2>Work in Progress</h2>
 			</div>
 			<div class="half-right grid-content">
 				<ul style="list-style-type:'&horbar; ' !important;">
-					<li>Conducting a user experience audit for a medical technology company</li>
+					<li>Reimagining the UX strategy and website design for a leading industrial vehicle manufacturer</li>
+					<li>Leading a user experience audit and UX strategy for a medical technology company</li>
 					<li>Designing digital and retail marketing materials for a Boston-based cannabis company</li>
-					<li>Providing design and frontend development support to a GovTech startup</li>
-					<li>Redesigning a website for a national pilot training academy</li>
 				</ul>
 			</div>
 		</section>
 	</div>
 
-	<h2 class="text-center brand-title mb-3 hidden" style="">What We&rsquo;ve Made</h2>
+	<h2 class="hidden mb-3 text-center brand-title" style="">What We&rsquo;ve Made</h2>
 	<section class="grid-home">
-		<div class="sample-lg-r hidden">
-			<div class="content order-2">
+		<div class="hidden sample-lg-r">
+			<div class="order-2 content">
 				<!-- <p class="text-xs uppercase">UX and Website Design</p> -->
 				<h2 class="mb-1"><a href="/work/latest/">Latest Work</a></h2>
 				<p>An inclusive, innovative platform for small businesses. A modern, resposive website redesign. And more…</p>
 			</div>
-			<div class="img img-hover order-1">
+			<div class="order-1 img img-hover">
 				<a href="/work/latest/">
 					<picture class="img">
 						<source srcset="/img/samples/latest/thumb.webp" media="(min-width: 50em)" type="image/webp">
@@ -100,7 +99,7 @@
 			</div>
 		</div>
 
-		<div class="sample-lg-l hidden">
+		<div class="hidden sample-lg-l">
 			<div class="img img-hover">
 				<a href="/work/right-triangle/">
 					<picture class="img">
@@ -119,13 +118,13 @@
 			</div>
 		</div>
 
-		<div class="sample-lg-r hidden" style="place-items:center;">
-			<div class="content order-2">
+		<div class="hidden sample-lg-r" style="place-items:center;">
+			<div class="order-2 content">
 				<p class="text-xs uppercase">UX and Website Design</p>
 				<h2 class="mb-1"><a href="/work/qwally/">Qwally</a></h2>
 				<p>A user experience designed to make inclusive contracting easy for minority-owned businesses and cities.</p>
 			</div>
-			<div class="img img-hover order-1">
+			<div class="order-1 img img-hover">
 				<a href="/work/qwally/">
 					<picture class="img">
 						<source srcset="/img/samples/qwally/thumb.webp" media="(min-width: 50em)" type="image/webp">
@@ -138,19 +137,19 @@
 			</div>
 		</div>
 
-    <section class="main grid-in mb-4">
+    <section class="mb-4 main grid-in">
       <div class="testimonial">
-				<div class="source pl-1 pr-1 order-2">
+				<div class="order-2 pl-1 pr-1 source">
 					<p><strong>Qwally</strong><br>Chris Offensend,<br>Co-Founder &amp; CEO</p>
 				</div>
-				<div class="quote pl-1 pr-1 order-1">
+				<div class="order-1 pl-1 pr-1 quote">
 					<p>"What made flow14 the easy choice for us was their focus on fundamentally understanding the problem to be solved and shepherding our team (and our clients) through a process to arrive at a solution that truly helped users accomplish their goals."</p>
         </div>
       </div>
     </section>
 
 
-		<div class="sample-sm-l hidden">
+		<div class="hidden sample-sm-l">
 			<div class="img img-hover">
 				<a href="/work/websites/">
 					<picture class="img">
@@ -171,37 +170,37 @@
 	</section>
 
 	<div class="grid">
-		<section class="main grid-in mb-4">
+		<section class="mb-4 main grid-in">
 			<div class="half-right grid-content">
 				<h3 class="color-orange">Ready for design that makes a difference for your company?</h3>
-				<a class="cta my-1" href="/contact/" style=""><h3 class="m-none">Let&rsquo;s Talk<span style="letter-spacing:-3px;">――――</span></h3></a>
+				<a class="my-1 cta" href="/contact/" style=""><h3 class="m-none">Let&rsquo;s Talk<span style="letter-spacing:-3px;">――――</span></h3></a>
 			</div>
 		</section>
 	</div>
 
 	<section class="grid my-2">
 		<div class="main grid-brand grid-brand-sm">
-			<h2 class="text-center brand-title mb-1 hidden" style="">Brands We&rsquo;ve Worked With:</h2>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/garmin-logo.svg" alt="Garmin logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/hallmark-logo.svg" alt="Hallmark logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w50" src="/img/brands/amc-logo.svg" alt="AMC logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/medtronic-logo.svg" alt="Medtronic logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/boston-scientific-logo.svg" alt="Boston Scientific logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w50" src="/img/brands/vista-outdoor-logo.svg" alt="Vista Outdoor logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/bushnell-logo.svg" alt=" logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/hv-logo-h.svg" alt="Happy Valley logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w50" src="/img/brands/deg-logo.svg" alt="DEG logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w100" src="/img/brands/grayduck-logo.svg" alt="Grayduck Health logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/inquest-logo.svg" alt="InQuest Marketing logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/sidebench-logo.svg" alt="Sidebench logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w50" src="/img/brands/qwally-logo.svg" alt="Qwally logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/get-konnected-logo.svg" alt="Get Konnected logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/hnc-living-logo.svg" alt="HNC Living logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/one-global-village.svg" alt="One Global Village logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/right-triangle-logo.svg" alt="Right Triangle logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/rounding-up-undies.svg" alt="Rounding Up Undies logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/vmc-group-logo.svg" alt="VMC Group logo."></div>
-			<div class="brand hidden" style=""><img loading="lazy" class="w75" src="/img/brands/stay-alfred-logo.svg" alt="Stay Alfred logo."></div>
+			<h2 class="hidden mb-1 text-center brand-title" style="">Brands We&rsquo;ve Worked With:</h2>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/garmin-logo.svg" alt="Garmin logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/hallmark-logo.svg" alt="Hallmark logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w50" src="/img/brands/amc-logo.svg" alt="AMC logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/medtronic-logo.svg" alt="Medtronic logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/boston-scientific-logo.svg" alt="Boston Scientific logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w50" src="/img/brands/vista-outdoor-logo.svg" alt="Vista Outdoor logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/bushnell-logo.svg" alt=" logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/hv-logo-h.svg" alt="Happy Valley logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w50" src="/img/brands/deg-logo.svg" alt="DEG logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w100" src="/img/brands/grayduck-logo.svg" alt="Grayduck Health logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/inquest-logo.svg" alt="InQuest Marketing logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/sidebench-logo.svg" alt="Sidebench logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w50" src="/img/brands/qwally-logo.svg" alt="Qwally logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/get-konnected-logo.svg" alt="Get Konnected logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/hnc-living-logo.svg" alt="HNC Living logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/one-global-village.svg" alt="One Global Village logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/right-triangle-logo.svg" alt="Right Triangle logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/rounding-up-undies.svg" alt="Rounding Up Undies logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/vmc-group-logo.svg" alt="VMC Group logo."></div>
+			<div class="hidden brand" style=""><img loading="lazy" class="w75" src="/img/brands/stay-alfred-logo.svg" alt="Stay Alfred logo."></div>
 		</div>
 	</section>
 
