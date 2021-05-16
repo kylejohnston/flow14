a6008ec1aff5f770ff8da0a952979057ee5a2fdf
Kyle Johnston <flow14@gmail.com>
2021-05-16 17:13:28 -0500

    Updated SVG xmlns to https

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <body>
 	<header class="r-fixed">
 			<div class="text-xl uppercase hamburger color-orange" style="position:absolute;z-index:201;">
-				<svg style="width:24px;height:24px;"fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16"></path></svg>
+				<svg style="width:24px;height:24px;"fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16"></path></svg>
 			</div>
 	</header>
 
