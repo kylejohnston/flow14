019b295cbbbaf0773bc53c9d18773673d8f17ded
Kyle Johnston <flow14@gmail.com>
2021-08-28 15:48:16 -0500

    Removed legacy tracking code

--- includes/foot.php
+++ includes/foot.php
@@ -6,9 +6,9 @@
 <!-- Analytics - - - - - - - - - - -->
 <!-- StatCounter -->
 	<script type="text/javascript">
-		var sc_project=651567; 
-		var sc_invisible=1; 
-		var sc_security="59987d53"; 
+		var sc_project=651567;
+		var sc_invisible=1;
+		var sc_security="59987d53";
 		var scJsHost = (("https:" == document.location.protocol) ?
 		"https://secure." : "http://www.");
 		document.write("<sc"+"ript type='text/javascript' src='" +
@@ -18,27 +18,3 @@
 
 <!-- HubSpot -->
 	<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/3292081.js"></script>
-
-<!-- Hotjar Tracking Code for http://www.flow14.com -->
-	<script>
-		(function(h,o,t,j,a,r){
-		  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-		  h._hjSettings={hjid:583449,hjsv:5};
-		  a=o.getElementsByTagName('head')[0];
-		  r=o.createElement('script');r.async=1;
-		  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-		  a.appendChild(r);
-		})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-	</script>
-
-<!-- ETC - - - - - - - - - - -->
-<!-- Pinterest -->
-	<script async type="text/javascript">
-	(function(d){
-	  var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
-	  p.type = 'text/javascript';
-	  p.async = true;
-	  p.src = '//assets.pinterest.com/js/pinit.js';
-	  f.parentNode.insertBefore(p, f);
-	}(document));
-	</script>
