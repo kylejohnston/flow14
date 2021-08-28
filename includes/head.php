288a20ffcd6f5150d3a69721a674683f76f909ba
Kyle Johnston <flow14@gmail.com>
2021-08-28 15:48:58 -0500

    Removed GA tracking

--- includes/head.php
+++ includes/head.php
@@ -14,13 +14,4 @@
 	<!-- Styles -->
 	<link rel="stylesheet" href="https://use.typekit.net/ayz0tps.css">
 	<link href="/css/main.css?202107_0001" rel="stylesheet">
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-92556-22', 'auto');
-	  ga('send', 'pageview');
-	</script>
 
